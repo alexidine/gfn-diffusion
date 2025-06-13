:> VarGrad vs TB test configs :> more reporting fixes :> options for scaling forward vs. backward training :> clarity upgrades including around configs :> major speedups around the buffer

energy_sampling/buffer.py
@@ -201,55 +201,54 @@ class CrystalReplayBuffer():
         else:
             self.dataset.extend(dataset)
 
-        if len(self.dataset) > self.buffer_size:  # todo consider sorting here by rank / diversity
-            self.dataset = self.dataset[-self.buffer_size:]
-            self.scores_np = self.dataset[-self.buffer_size:]
-
         if not hasattr(self, 'scores_np'):
             self.scores_np = self.energy_function.prebuilt_sample_to_reward(self.dataset, temperature=torch.ones(
                 len(self.dataset))).detach().cpu().view(-1).numpy()
+
         else:
             self.scores_np = np.concatenate([
                 self.scores_np,
                 self.energy_function.prebuilt_sample_to_reward(
                     dataset,
                     temperature=torch.ones(len(dataset))).detach().cpu().view(-1).numpy()
-                ]
-            )
+            ])
+            self.build_sampler()
 
-        if self.prioritized == 'rank':  # todo add a diversity-type sampler
-            ranks = np.argsort(np.argsort(-1 * self.scores_np))
-            weights = 1.0 / (1e-2 * len(self.scores_np) + ranks)
-            self.sampler = torch.utils.data.WeightedRandomSampler(
-                weights=weights, num_samples=len(self.scores_np), replacement=True
-            )
+        if len(self.dataset) > self.buffer_size:
+            if hasattr(self, 'sampler'):
+                inds_to_keep = list(self.sampler)[:self.buffer_size]
+            else:
+                inds_to_keep = np.arange(len(self.dataset) - self.buffer_size, len(self.dataset))
 
-            self.loader = DataLoader(
-                self.dataset,
-                batch_size=self.batch_size,
-                sampler=self.sampler,
-                num_workers=0,
-                pin_memory=True,
-                drop_last=False)
-        else:
-            weights = 1.0
-            self.sampler = torch.utils.data.WeightedRandomSampler(
-                weights=weights, num_samples=len(self.scores_np), replacement=True
-            )
+            self.dataset = [self.dataset[ind] for ind in inds_to_keep]
+            if hasattr(self, 'scores_np'):
+                self.scores_np = np.array([self.scores_np[ind] for ind in inds_to_keep])
+                self.build_sampler()
 
-            self.loader = DataLoader(
-                self.dataset,
-                batch_size=self.batch_size,
-                sampler=self.sampler,
-                num_workers=0,
-                pin_memory=True,
-                drop_last=False)
+        self.loader = DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            sampler=self.sampler,
+            num_workers=0,
+            pin_memory=True,
+            drop_last=False)
 
     def __len__(self):
         if self.dataset is None:
             return 0
         else:
             return len(self.dataset)
+
+    def build_sampler(self):
+        if self.prioritized == 'rank':
+            ranks = np.argsort(np.argsort(-1 * self.scores_np))
+            weights = 1.0 / (1e-2 * len(self.scores_np) + ranks)
+        else:
+            weights = torch.ones(len(self.scores_np))
+
+        self.sampler = torch.utils.data.WeightedRandomSampler(
+            weights=weights, num_samples=len(self.scores_np), replacement=True
+        )
 
     def sample(self,
                temperature: Optional[torch.tensor] = None,
@@ -270,7 +269,8 @@ class CrystalReplayBuffer():
 
         condition = self.energy_function.get_conditioning_tensor(sample)
         temperature = 10 ** condition[:, 0]  # first dimension is the log temperature
-        reward = self.energy_function.prebuilt_sample_to_reward(sample, temperature)  # recompute reward in case parameters have changed
+        reward = self.energy_function.prebuilt_sample_to_reward(sample,
+                                                                temperature)  # recompute reward in case parameters have changed
 
         if return_conditioning:
             return sample.cell_params_to_gen_basis(), reward, sample, condition

energy_sampling/gflownet_losses.py
@@ -26,7 +26,7 @@ def fwd_tb(initial_state, gfn, log_reward_fn, mol_batch, exploration_std=None, r
     log_pf = log_pfs.sum(-1)
     log_pb = log_pbs.sum(-1)
     log_ratio = log_pf + log_fs[:, 0] - log_pb - log_r
-    log_ratio = log_ratio.clip(min=-10, max=10)  # clip extremely large losses
+    #log_ratio = log_ratio.clip(min=-10, max=10)  # clip extremely large losses
     loss = 0.5 * (log_ratio ** 2)
     if return_exp:
         return loss.mean(), states, log_pfs, log_pbs, log_r, crystal_batch
@@ -37,7 +37,7 @@ def fwd_tb(initial_state, gfn, log_reward_fn, mol_batch, exploration_std=None, r
 def bwd_tb(initial_state, gfn, log_r, exploration_std=None, condition=None):
     states, log_pfs, log_pbs, log_fs = gfn.get_trajectory_bwd(initial_state, exploration_std, condition)
     log_ratio = log_pfs.sum(-1) + log_fs[:, 0] - log_pbs.sum(-1) - log_r
-    log_ratio = log_ratio.clip(min=-10, max=10)  # clip extremely large losses
+    #log_ratio = log_ratio.clip(min=-10, max=10)  # clip extremely large losses
     loss = 0.5 * (log_ratio ** 2)
 
     return loss.mean()
@@ -92,7 +92,7 @@ def fwd_tb_avg_cond(initial_state, gfn, log_reward_fn, mol_batch, exploration_st
     log_Z = (log_r + log_pb - log_pf).view(repeats, -1).mean(dim=0, keepdim=True)
     # minimize the variance over repeats w.r.t., the norm
     loss = log_Z + (log_pf - log_r - log_pb).view(repeats, -1)
-    loss = loss.clip(min=-10, max=10)  # clip extreme outliers for stability
+    #loss = loss.clip(min=-10, max=10)  # clip extreme outliers for stability
     if return_exp:
         return 0.5 * (loss ** 2).mean(), states, log_pfs, log_pbs, log_r, crystal_batch
     else:
@@ -109,7 +109,7 @@ def bwd_tb_avg_cond(initial_state, gfn, log_r, exploration_std=None, condition=N
     log_pb = log_pbs.sum(-1)
     log_Z = (log_r + log_pb - log_pf).view(repeats, -1).mean(dim=0, keepdim=True)
     loss = log_Z + (log_pf - log_r - log_pb).view(repeats, -1)
-    loss = loss.clip(min=-10, max=10)  # clip extreme outliers for stability
+    #loss = loss.clip(min=-10, max=10)  # clip extreme outliers for stability
     return 0.5 * (loss ** 2).mean()
 
 