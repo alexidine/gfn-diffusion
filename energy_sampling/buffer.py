125d46aec4a5da604da83bfb9ec50b8425cca714
InfluenceFunctional <mjakilgour@gmail.com>
2025-06-23 16:54:53 -0400

    :> buffer indexing fix

--- energy_sampling/buffer.py
+++ energy_sampling/buffer.py
@@ -45,9 +45,10 @@ class CrystalReplayBuffer():
             else:
                 if filter_diversity:
                     new_x = collate_data_list(data_list).cell_params_to_gen_basis()
-                    rands = np.random.choice(len(self.dataset), self.diversity_check_size, replace=False)
+                    rands = torch.from_numpy(
+                        np.random.choice(len(self.dataset), self.diversity_check_size, replace=False))
                     new_x_dists = torch.cdist(self.x[rands], new_x)
-                    new_x_inds_to_keep = new_x_dists.amin(dim=0) >= diversity_cutoff
+                    new_x_inds_to_keep = torch.argwhere(new_x_dists.amin(dim=0) >= diversity_cutoff).flatten()
                     data_list = [data_list[ind] for ind in new_x_inds_to_keep]
 
                 self.dataset.extend(list(data_list))
@@ -57,6 +58,7 @@ class CrystalReplayBuffer():
                         data_list,
                         temperature=torch.ones(len(data_list))).detach().cpu().view(-1).numpy())
                 )
+                assert len(self.dataset) == len(self.x) == len(self.scores_np_list)
 
             if len(self) > self.buffer_size:  # pare down buffer
                 inds_to_keep = self.sample_indices(self.buffer_size, replace=False)
