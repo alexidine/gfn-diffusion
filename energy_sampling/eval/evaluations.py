:> reporting update :> crystal chunk generator update :> adjusted variance clipping behavior :> fixed rotmat2rotvec

energy_sampling/eval/evaluations.py
@@ -176,10 +176,22 @@ def generate_fwd_figs(buffer, energy_function,
     fig_dict['Lattice Latents Trajectories'] = visualize_latent_trajs(flow_states.cpu().detach().numpy(),
                                                                       20,
                                                                       log_r.cpu().detach().numpy())
-    fig_dict['Lattice Features Distribution'] = simple_cell_hist(sample_batch, buffer_cell_params,
+    fig_dict['Lattice Features Distribution'], cell_klds = simple_cell_hist(sample_batch, buffer_cell_params,
                                                                  n_kde_points=200, bw_ratio=10, mode='cell')
-    fig_dict['Lattice Latents Distribution'] = simple_cell_hist(sample_batch, buffer_latent_params,
+    fig_dict['Lattice Latents Distribution'], latent_klds = simple_cell_hist(sample_batch, buffer_latent_params,
                                                                 n_kde_points=200, bw_ratio=10, mode='latent')
+
+    lattice_features = ['cell_a', 'cell_b', 'cell_c',
+                        'cell_alpha', 'cell_beta', 'cell_gamma',
+                        'aunit_x', 'aunit_y', 'aunit_z',
+                        'orientation_1', 'orientation_2', 'orientation_3']
+
+    for ind, feat in enumerate(lattice_features):
+        fig_dict[f'{feat} Cell KLD'] = cell_klds[ind]
+        fig_dict[f'{feat} Latent KLD'] = latent_klds[ind]
+    fig_dict['Mean Cell KLD'] = np.mean(cell_klds)
+    fig_dict['Mean Latent KLD'] = np.mean(latent_klds)
+
     log_T = len(torch.unique(log_T_tensor)) > 1
     fig_dict['Sample Scatter'] = simple_cell_scatter_fig(
         sample_batch,

energy_sampling/eval/offline_evaluation.py
@@ -124,6 +124,12 @@ if not os.path.exists('csd_results.npy') or override_resample:
             encoder
         )
     np.save('csd_results', csd_sampling_dict)
+    print('getting RDFs')
+    csd_rdf_dists, rr = sample_csd_rdf_dists(csd_mols,
+                                             csd_sampling_dict,
+                                             args.eval_batch_size,
+                                             args.device)
+    np.save('csd_rdfs', csd_rdf_dists.cpu().detach().numpy())
 
 """
 Reporting
@@ -173,17 +179,7 @@ ref_energies = csd_clusters.compute_silu_energy()
 
 # load samples
 csd_sampling_dict = np.load('csd_results.npy', allow_pickle=True).item()
-
-# get rdf dists
-print('getting RDFs')
-if not os.path.exists('csd_rdfs.npy'):
-    rdf_dists, rr = sample_csd_rdf_dists(csd_mols,
-                                         csd_sampling_dict,
-                                         args.eval_batch_size,
-                                         args.device)
-    np.save('csd_rdfs', rdf_dists.cpu().detach().numpy())
-else:
-    rdf_dists = np.load('csd_rdfs.npy',allow_pickle=True)
+csd_rdf_dists = np.load('csd_rdfs.npy', allow_pickle=True)
 
 # funnel figs
 if args.show_figs:
@@ -193,11 +189,11 @@ if args.show_figs:
         funnel_figs.append(crystal_sample_funnel_plot(
             packing_coeff=samples['densities'].flatten(),
             energies=samples['energies'].flatten(),
-            dists=torch.tensor(rdf_dists)[ind],
+            dists=torch.tensor(csd_rdf_dists)[ind],
             ref_energies=torch.tensor([ref_energies[ind]]),
             ref_packing_coeff=csd_clusters[ind].packing_coeff
         ))
-    [f.show() for f in funnel_figs]
+    [f.show(renderer='browser') for f in funnel_figs]
 
 # Divergence between lattice distance sets
 js_divs = np.array(sample_csd_lattice_divs(csd_mols, csd_sampling_dict))
@@ -239,7 +235,7 @@ for ind in range(len(csd_mols)):
         import plotly.graph_objects as go
 
         go.Figure(
-            go.Scatter(x=scat_dists.flatten(), y=js_dists, mode='markers', marker_color=np.log10(rdf_dists[ind]))).show(
+            go.Scatter(x=scat_dists.flatten(), y=js_dists, mode='markers', marker_color=np.log10(csd_rdf_dists[ind]))).show(
             renderer='browser', marker_colorscale='viridis')
 
 aa = 1

energy_sampling/models/gfn.py
@@ -47,6 +47,7 @@ class GFN(nn.Module):
         self.dt = 1. / trajectory_length
         self.log_var_range = log_var_range
         self.device = device
+        self.var_clip = 16
 
         if self.conditional_flow_model:
             self.conditions_embedding_model = scalarMLP(input_dim=conditions_dim,
@@ -90,7 +91,7 @@ class GFN(nn.Module):
                 logvar = logvar_i
             else:
                 logvar = torch.tanh(logvar_i / self.log_var_range) * self.log_var_range
-        return mean, (logvar + np.log(self.pf_std_per_traj) * 2.0).clip(min=-8, max=8)
+        return mean, (logvar + np.log(self.pf_std_per_traj) * 2.0).clip(min=-self.var_clip, max=self.var_clip)
 
     def predict_next_state(self, s, t, condition_embedding):
         s_new = self.forward_policy(self.s_model(s, condition_embedding), self.t_model(t))
@@ -136,7 +137,7 @@ class GFN(nn.Module):
             if i > 0:  # variance is exactly zero for the first step, so we can't use it
                 # back_var = ((self.pf_std_per_traj ** 2) * (dts * ts[:, i] / ts[:, i + 1]).unsqueeze(
                 #     1) * back_var_correction)
-                var = (back_var_correction + np.log(self.pf_std_per_traj) * 2.0).clip(min=-8, max=8).exp()
+                var = (back_var_correction + np.log(self.pf_std_per_traj) * 2.0).clip(min=-self.var_clip, max=self.var_clip).exp()
                 back_var = var * (dts * ts[:, i] / ts[:, i + 1]).unsqueeze(1)
                 noise_backward = (current_state - back_mean) / back_var.sqrt()
                 logpb[:, i] = -0.5 * (noise_backward ** 2 + logtwopi + back_var.log()).sum(1)
@@ -188,7 +189,7 @@ class GFN(nn.Module):
                 # back_var = ((self.pf_std_per_traj ** 2) *
                 #             (dts * ts[:, trajectory_length - i - 1] / ts[:, trajectory_length - i]).unsqueeze(
                 #                 1) * back_var_correction)
-                var = (back_var_correction + np.log(self.pf_std_per_traj) * 2.0).clip(min=-8, max=8).exp()
+                var = (back_var_correction + np.log(self.pf_std_per_traj) * 2.0).clip(min=-self.var_clip, max=self.var_clip).exp()
                 back_var = var * (dts * ts[:, trajectory_length - i - 1] / ts[:, trajectory_length - i]).unsqueeze(1)
                 prev_state = self.bwd_propagate(back_mean, back_var, current_state, detach_traj)
                 noise_backward = (prev_state - back_mean) / back_var.sqrt()