:> removed smoothing loss :> fixed OOM issue :> accelerated crystal instantiation :> ellipsoid overlaps calculation now more stable and dramatically cheaper :> added option for dynamic forward/backward balance according to prior coverage

energy_sampling/buffer.py
@@ -18,7 +18,6 @@ class CrystalReplayBuffer:
                  rank_weight=1e-2,
                  prioritized=None,
                  keep_initial_samples: bool = False,
-                 gpu_available: bool = False,
                  diversity_coeff: float = 0.0,
                  ):
         self.buffer_size = buffer_size
@@ -37,7 +36,6 @@ class CrystalReplayBuffer:
         self.x = None
         self.diversity_check_size = 1000
         self.original_dataset_inds = None
-        self.gpu_available = gpu_available
         self.diversity_coeff = diversity_coeff
 
     def add(self,
@@ -67,13 +65,13 @@ class CrystalReplayBuffer:
                 if len(data_list) > 0:
                     new_data_batch = collate_data_list(data_list)
 
-                    new_x_tensor = new_data_batch.cell_params_to_gen_basis().to('cuda' if self.gpu_available else 'cpu')
+                    new_x_tensor = new_data_batch.cell_params_to_gen_basis().to(self.device)
                     scores = self.energy_function.prebuilt_sample_to_reward(new_data_batch,
                                                                             temperature=torch.ones(len(new_data_batch))
                                                                             ).cpu().detach().numpy()
                     scores_list = list(scores)
 
-                    ref_x_tensor = torch.stack(self.x_list).to('cuda' if self.gpu_available else 'cpu')
+                    ref_x_tensor = torch.stack(self.x_list).to(self.device)
                     min_buffer_dist = torch.cdist(ref_x_tensor, new_x_tensor).amin(0)
                     new_x_tensor = new_x_tensor.cpu()
 
@@ -134,6 +132,7 @@ class CrystalReplayBuffer:
                                                            ))
         return inds
 
+    @torch.inference_mode()
     def get_sampler_weights(self,
                             diversity_coeff,
                             eps: float = 1e-6,
@@ -145,16 +144,16 @@ class CrystalReplayBuffer:
 
         scores = np.array(self.rewards_list)
         if diversity_coeff > 0:
-            x_tensor = torch.stack(self.x_list).to('cuda' if self.gpu_available else 'cpu')
-            if len(x_tensor) > 5000:
-                subsample_inds = np.random.choice(len(self), 5000, replace=False)
+            x_tensor = torch.stack(self.x_list).to(self.device)
+            if len(x_tensor) > 1000:
+                subsample_inds = np.random.choice(len(self), 1000, replace=False)
                 scores -= diversity_coeff * ((compute_sample_overlap(x_tensor[subsample_inds].float(),
                                                                      x_tensor.float(),
-                                                                     ga=0.1,
+                                                                     ga=0.01,
                                                                      agg='sum')).cpu().detach().numpy() - 1)  # subtract self contribution
             else:
                 scores -= diversity_coeff * ((compute_sample_overlap(x_tensor.float(),
-                                                                     ga=0.1,
+                                                                     ga=0.01,
                                                                      agg='sum')).cpu().detach().numpy() - 1)  # subtract self contribution
 
         if method == 'rank':
@@ -169,7 +168,7 @@ class CrystalReplayBuffer:
 
         return weights_i / np.sum(weights_i)  # enforce explicit normalization
 
-    @torch.no_grad()
+    @torch.inference_mode()
     def sample(self,
                override_batch: Optional[int] = None,
                return_preload: Optional[bool] = False,
@@ -199,9 +198,8 @@ class CrystalReplayBuffer:
                                                                                     sg_inds=sample.sg_ind)
         sample.sg_ind = sg_inds
         temperature = 10 ** T_tensor  # first dimension is the log temperature
-        with torch.no_grad():
-            reward = self.energy_function.prebuilt_sample_to_reward(
-                sample, temperature)  # recompute reward in case parameters have changed
+        reward = self.energy_function.prebuilt_sample_to_reward(
+            sample, temperature)  # recompute reward in case parameters have changed
 
         return sample.cell_params_to_gen_basis(), reward, sample, condition
 

energy_sampling/energies/molecular_crystal.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn.functional as F
 
 from mxtaltools.constants.space_group_feature_tensor import SG_FEATURE_TENSOR
+from mxtaltools.constants.space_group_info import SYM_OPS
 from mxtaltools.dataset_utils.data_classes import MolCrystalData
 from mxtaltools.dataset_utils.utils import collate_data_list
 from .base_set import BaseSet
@@ -176,8 +177,6 @@ class MolecularCrystal(BaseSet):
 
     def generator_energy(self, cluster_batch, raw_latents=None):
         ens_dict = {}
-        if cluster_batch.device != self.device:
-            cluster_batch = cluster_batch.to(self.device)
 
         latents = cluster_batch.cell_params_to_gen_basis()
         if raw_latents is not None:
@@ -272,6 +271,7 @@ class MolecularCrystal(BaseSet):
                 ens_dict)  # softly bound from above  #crystal_energy.clip(min=-self.energy_clip, max=self.energy_clip)
         #return soft_clip(total_energy, 0).clip(max=self.energy_clip), ens_dict  # softly bound from above  #crystal_energy.clip(min=-self.energy_clip, max=self.energy_clip)
 
+    @torch.inference_mode()
     def prebuilt_sample_to_reward(self, crystals, temperature):
         """
         For pre-built, pre-scored crystal, generate the approriate reward for this point in training.
@@ -284,17 +284,16 @@ class MolecularCrystal(BaseSet):
         else:
             crystal_batch = crystals
 
-        with torch.no_grad():
-            energy, _ = self.generator_energy(crystal_batch)
+        energy, _ = self.generator_energy(crystal_batch)
 
         if torch.is_tensor(temperature):
-            sample_temperature = temperature.to(self.device)
+            sample_temperature = temperature.to(crystal_batch.device)
         elif isinstance(temperature, float) or isinstance(temperature, int):
-            sample_temperature = temperature * torch.ones_like(energy, device=self.device)
+            sample_temperature = temperature * torch.ones_like(energy, device=crystal_batch.device)
         else:
             assert False
 
-        return (-energy / sample_temperature).detach()
+        return -energy / sample_temperature
 
     def energy(self,
                x,
@@ -320,12 +319,12 @@ class MolecularCrystal(BaseSet):
             return energy / sample_temperature
 
     def init_blank_crystal_batch(self, mol_batch):  # todo no possible way this is the most efficient way to do this
-
-        ones3 = torch.ones(3, device=self.device)
-        zeros1 = torch.zeros(1, device=self.device)
-
+        ones3 = torch.ones(3, device='cpu')
+        zeros1 = torch.zeros(1, device='cpu')
+        eye3 = torch.eye(3, device='cpu')[None, :]
+        ones1 = torch.ones(1, device='cpu')
         if self.energy_function in ['ellipsoid_overlap', 'combo']:
-            overlap_tensor = torch.zeros(1, device=self.device)
+            overlap_tensor = torch.zeros(1, device='cpu')
         else:
             overlap_tensor = None
 
@@ -334,19 +333,18 @@ class MolecularCrystal(BaseSet):
         else:
             sgs = [self.space_groups[0] for _ in range(mol_batch.num_graphs)]
 
-        crystal_batch = collate_data_list([MolCrystalData(
-            molecule=mol_batch[ind].clone(),  # must be cloned
-            sg_ind=sgs[ind],
-            aunit_handedness=torch.ones(1),
-            cell_lengths=torch.ones(3, device=self.device),
-            # if we don't put dummies in here, later ops to_data_list fail
-            # but if we do put dummies in here, it does box analysis one-by-one which is super slow
+        sg_cache = {}
+        for sg in set(sgs):
+            sg_cache[sg] = np.stack(SYM_OPS[int(sg)])
+
+        base_xtal = MolCrystalData(
+            skip_box_analysis=True,
+            aunit_handedness=ones1,
+            cell_lengths=ones3,
             cell_angles=ones3,
             aunit_centroid=ones3,
             aunit_orientation=ones3,
-            skip_box_analysis=True,
             silu_pot=zeros1,
-            packing_coeff=zeros1,
             lj_pot=zeros1,
             scaled_lj_pot=zeros1,
             es_pot=zeros1,
@@ -357,7 +355,23 @@ class MolecularCrystal(BaseSet):
             core_energy=zeros1,
             lj_energy=zeros1,
             bounding_energy=zeros1,
-        ) for ind in range(len(mol_batch))]).to(self.device)
+            T_fc =eye3,
+            T_cf = eye3,
+            cell_volume = zeros1,
+            packing_coeff = zeros1,
+            density = zeros1,
+        )
+        crystal_list = []
+        for ind in range(mol_batch.num_graphs):
+            crystal = base_xtal.clone()
+            crystal.set_mol_attrs(mol_batch[ind].clone())
+            crystal.set_sg_attrs(is_well_defined=True,
+                                 nonstandard_symmetry=False,
+                                 sg_ind=sgs[ind],
+                                 symmetry_operators=sg_cache[sgs[ind]])
+            crystal_list.append(crystal)
+
+        crystal_batch = collate_data_list(crystal_list).to(self.device)
 
         return crystal_batch
 

energy_sampling/eval/evaluations.py
@@ -37,7 +37,7 @@ def eval_step(energy_function,
     gfn_model.eval()
 
     (flow_states, samples, log_r, log_Z, log_Z_lb,
-     log_Z_learned, sample_batch, condition, log_pfs, log_pbs, log_fs,
+     log_Z_learned, sample_batch, condition, log_pfs, log_pbs, log_flow,
      f_means_f, f_vars_f, f_means_b, f_vars_b,
      log_T_tensor) = log_partition_function(
         init_state, gfn_model, discretizer, energy_function, mol_batch)
@@ -55,7 +55,7 @@ def eval_step(energy_function,
                                      flow_states,
                                      gfn_model,
                                      init_state,
-                                     log_fs,
+                                     log_flow,
                                      log_pbs,
                                      log_pfs,
                                      log_r,
@@ -108,14 +108,14 @@ def conditional_eval_step(energy_function,
     cond_inds = torch.tensor([_ for _ in range(mols_to_sample)] * (samples_per_mol), dtype=torch.long)
 
     (flow_states, samples, log_r, log_Z, log_Z_lb,
-     log_Z_learned, sample_batch, condition, log_pfs, log_pbs, log_fs,
+     log_Z_learned, sample_batch, condition, log_pfs, log_pbs, log_flow,
      f_means_f, f_vars_f, f_means_b, f_vars_b,
      log_T_tensor) = log_partition_function(
         init_state, gfn_model, discretizer, energy_function, mol_batch)
 
     metrics = {}
     fig_dict = conditional_fwd_figs(
-        log_fs,
+        log_flow,
         log_pbs,
         log_pfs,
         log_r,
@@ -154,7 +154,7 @@ def log_crystals(sample_batch):
 def generate_fwd_figs(buffer, energy_function,
                       condition, flow_states,
                       gfn_model, init_state,
-                      log_fs, log_pbs, log_pfs, log_r,
+                      log_flow, log_pbs, log_pfs, log_r,
                       f_vars_f, f_means_f, f_vars_b, f_means_b, sample_batch,
                       log_T_tensor):
     fig_dict = {}
@@ -197,7 +197,7 @@ def generate_fwd_figs(buffer, energy_function,
     #     fig_dict['temp/Learned Z vs T'] = Z_vs_T_fig(gfn_model, init_state)
     #     fig_dict['temp/T vs Energy'] = T_vs_E_fig(condition, sample_batch)
     traj_param_logging(f_means_b, f_means_f, f_vars_b, f_vars_f, fig_dict, flow_states, log_pbs, log_pfs, log_r)
-    fig_dict['TB Parity Plot'], fig_dict['Forward TB R Value'] = flow_parity_plot(log_r, log_fs[:, 0], log_pbs, log_pfs)
+    fig_dict['TB Parity Plot'], fig_dict['Forward TB R Value'] = flow_parity_plot(log_r, log_flow, log_pbs, log_pfs)
     fig_dict['VG Error'] = vargrad_error(log_r, log_pbs, log_pfs)  # todo tune this up for conditional modelling
     fig_dict['Lattice Latents Trajectories'] = visualize_latent_trajs(flow_states.cpu().detach().numpy(),
                                                                       20,
@@ -207,7 +207,7 @@ def generate_fwd_figs(buffer, energy_function,
     fig_dict['Lattice Latents Distribution'], latent_klds = simple_cell_hist(sample_batch, buffer_latent_params,
                                                                              n_kde_points=200, bw_ratio=10,
                                                                              mode='latent')
-    fig_dict['Pf Parity'], fig_dict['Pf Parity R Value'] = Pf_alignment_fig(log_pfs, log_pbs, log_r, log_fs)
+    fig_dict['Pf Parity'], fig_dict['Pf Parity R Value'] = Pf_alignment_fig(log_pfs, log_pbs, log_r, log_flow)
 
     log_buffer_kld(cell_klds, fig_dict, latent_klds)
 
@@ -266,11 +266,11 @@ def known_mode_coverage(energy_function, fig_dict, std_cell_params):
         fig_dict['Soft Mode Coverage'] = coverage_per_mode.mean()
 
 
-def conditional_fwd_figs(log_fs, log_pbs, log_pfs, log_r,
+def conditional_fwd_figs(log_flow, log_pbs, log_pfs, log_r,
                          sample_batch, cond_inds,
                          log_T_tensor):
     fig_dict = {}
-    fig_dict['Conditional TB Parity Plot'], _ = conditional_flow_parity_plot(log_r, log_fs[:, 0], log_pbs, log_pfs,
+    fig_dict['Conditional TB Parity Plot'], _ = conditional_flow_parity_plot(log_r, log_flow, log_pbs, log_pfs,
                                                                              cond_inds)
     fig_dict['Conditional VG Error'] = conditional_vargrad_error(log_r, log_pbs,
                                                                  log_pfs, cond_inds)
@@ -790,7 +790,7 @@ def generate_bwd_figs(fig_dict, buffer, gfn_model, init_state, discretizer):
     terminal_state, b_log_r, crystal_batch, condition = buffer.sample(
         override_batch=len(init_state),
     override_sampler=None)
-    (backward_flow_states, b_log_pfs, b_log_pbs, b_log_fs,
+    (backward_flow_states, b_log_pfs, b_log_pbs, b_log_flow,
      b_means_f, b_vars_f, b_means_b, b_vars_b) = gfn_model.get_trajectory_bwd(
         terminal_state.to(gfn_model.device), discretizer, condition.to(gfn_model.device), return_gauss_params=True)
 
@@ -799,8 +799,8 @@ def generate_bwd_figs(fig_dict, buffer, gfn_model, init_state, discretizer):
         n_trajs=20, log_r=b_log_r.cpu().detach().numpy())
 
     fig_dict['Backward Pf vs Pb'] = Pf_vs_Pb_fig(b_log_pfs, b_log_pbs, b_log_r)
-    fig_dict['Backward TB Parity Plot'], fig_dict['Backward TB R Value'] = flow_parity_plot(b_log_r.to(b_log_fs.device),
-                                                                                            b_log_fs[:, 0], b_log_pbs,
+    fig_dict['Backward TB Parity Plot'], fig_dict['Backward TB R Value'] = flow_parity_plot(b_log_r.to(b_log_flow.device),
+                                                                                            b_log_flow, b_log_pbs,
                                                                                             b_log_pfs)
     fig_dict['Backward Gauss Params'] = mean_var_fig(b_vars_f, b_means_f,
                                                      b_vars_b, b_means_b)
@@ -811,7 +811,7 @@ def generate_bwd_figs(fig_dict, buffer, gfn_model, init_state, discretizer):
 
     fig_dict['Bwd Traj Mean Step Sizes'] = mean_flow_step_sizes(backward_flow_states)
 
-    log_weight = b_log_r + b_log_pbs.sum(-1) - b_log_pfs.sum(-1)
+    log_weight = b_log_r + b_log_pbs.sum(-1).cpu() - b_log_pfs.sum(-1).cpu()
     log_Z = logmeanexp(log_weight)
     log_Z_lb = log_weight.mean()
     fig_dict['Bwd Empirical log Z'] = log_Z.cpu().detach().numpy()
@@ -1013,9 +1013,9 @@ def Pf_vs_R_fig(pf, log_r):
     return fig, r_value
 
 
-def Pf_alignment_fig(log_pfs, log_pbs, log_r, log_fs):
+def Pf_alignment_fig(log_pfs, log_pbs, log_r, log_flow):
     x = log_pfs.sum(-1).cpu().detach().numpy()
-    y = (log_r + log_pbs.sum(-1) - log_fs[:, 0]).cpu().detach().numpy()
+    y = (log_r + log_pbs.sum(-1) - log_flow).cpu().detach().numpy()
     r_value, _ = pearsonr(x, y)
 
     fig = go.Figure()

energy_sampling/eval/utils.py
@@ -68,7 +68,7 @@ def sample_from_generator(
                 batch_inds = np.arange(b_ind * batch_size, (b_ind + 1) * batch_size)
                 mol_batch = collate_data_list([mol_list[ind] for ind in batch_inds]).to(device)
                 (flow_states, samples, log_r, log_Z, log_Z_lb,
-                 log_Z_learned, sample_batch, condition, log_pfs, log_pbs, log_fs,
+                 log_Z_learned, sample_batch, condition, log_pfs, log_pbs, log_flow,
                  f_means_f, f_vars_f, f_means_b, f_vars_b,
                  log_T_tensor) = log_partition_function(
                     init_state, gfn_model, discretizer, energy_function, mol_batch)
@@ -85,8 +85,9 @@ def sample_from_generator(
 @torch.no_grad()
 def log_partition_function(initial_state, gfn, discretizer, energy_function, mol_batch):
     log_T_tensor, sg_inds, condition = energy_function.get_conditioning_tensor(mol_batch)
+    condition = condition.to(gfn.device)
     mol_batch.sg_ind = sg_inds
-    (states, log_pfs, log_pbs, log_fs,
+    (states, log_pfs, log_pbs, log_flow,
      means_f, logvars_f, means_b, logvars_b) = gfn.get_trajectory_fwd(initial_state,
                                                                       discretizer,
                                                                       None,
@@ -100,12 +101,12 @@ def log_partition_function(initial_state, gfn, discretizer, energy_function, mol
 
     log_Z = logmeanexp(log_weight)
     log_Z_lb = log_weight.mean()
-    log_Z_learned = log_fs[:, 0].mean()
+    log_Z_learned = log_flow.mean()
 
     return (states, states[:, -1],
             log_r, log_Z, log_Z_lb, log_Z_learned,
             sample_batch, condition,
-            log_pfs, log_pbs, log_fs,
+            log_pfs, log_pbs, log_flow,
             means_f, logvars_f, means_b, logvars_b,
             log_T_tensor)
 

energy_sampling/gflownet_losses.py
@@ -24,17 +24,26 @@ def diagonal_gaussian_log_density(x, mu, sigma):
 
 
 def get_loss_reward(log_T_tensor, log_reward_fn, mol_batch, return_exp, states, no_grad: bool = True):
-    if log_T_tensor is not None:
-        log_temperature = log_T_tensor
+    log_temperature = log_T_tensor if log_T_tensor is not None else None
+
+    x_T = states[:, -1]
+    if no_grad:
+        x_T = x_T.detach()
+        ctx = torch.inference_mode()
     else:
-        log_temperature = None
-    with torch.set_grad_enabled(not no_grad):
+        ctx = torch.enable_grad()
+
+    with ctx:
         if return_exp:
-            log_r, crystal_batch = log_reward_fn(states[:, -1], mol_batch, log_temperature, return_exp)
-            crystal_batch = crystal_batch.detach()
+            log_r, crystal_batch = log_reward_fn(x_T, mol_batch, log_temperature, return_exp)
+            crystal_batch = crystal_batch.detach().to('cpu')
         else:
             log_r = log_reward_fn(states[:, -1], mol_batch, log_temperature, return_exp)
             crystal_batch = None
+
+    if no_grad:
+        log_r = log_r.detach()
+
     return crystal_batch, log_r
 
 
@@ -91,13 +100,14 @@ def get_gfn_forward_loss(loss_coeffs,
     else:
         keep_grads = False
 
-    (states, log_pfs, log_pbs, log_fs,
-     means_f, logvars_f, means_b, logvars_b) = gfn.get_trajectory_fwd(initial_state,
+    condition = condition.to(gfn.device)
+    log_T_tensor = log_T_tensor.to(gfn.device)
+    (states, log_pfs, log_pbs, log_flow) = gfn.get_trajectory_fwd(initial_state,
                                                                       discretizer,
                                                                       exploration_std,
                                                                       condition,
                                                                       detach_traj=not keep_grads,
-                                                                      return_gauss_params=True,
+                                                                      return_gauss_params=False,
                                                                       )
 
     crystal_batch, log_r = get_loss_reward(log_T_tensor,
@@ -109,7 +119,6 @@ def get_gfn_forward_loss(loss_coeffs,
                                            )
     log_pf = log_pfs.sum(-1)
     log_pb = log_pbs.sum(-1)
-    log_flow = log_fs[:, 0]
 
     losses = []
     """greedy loss"""
@@ -125,11 +134,6 @@ def get_gfn_forward_loss(loss_coeffs,
         weight *= len(weight)
         reinforce_loss = -power_saturate(weight * log_pf, 0.7)
         losses.append(reinforce_loss * loss_coeffs.reinforce)
-
-    """trajectory smoothing loss"""
-    if loss_coeffs.smoothed > 0:
-        smoothness_loss = normed_smoothness_loss(torch.stack([means_f, logvars_f, means_b, logvars_b])).mean(dim=0)
-        losses.append(smoothness_loss * loss_coeffs.smoothed)
 
     """VarGrad - lower bound loss"""
     if loss_coeffs.vg_lb > 0:
@@ -186,8 +190,6 @@ def get_gfn_forward_loss(loss_coeffs,
             loss_dict['greedy'] = greedy_loss.mean().detach()
         if loss_coeffs.reinforce > 0:
             loss_dict['reinforce'] = reinforce_loss.mean().detach()
-        if loss_coeffs.smoothed > 0:
-            loss_dict['smoothed'] = smoothness_loss.mean().detach()
         if loss_coeffs.tb > 0:
             loss_dict['tb'] = tb_loss.mean().detach()
         if loss_coeffs.vg_lb > 0:
@@ -208,9 +210,150 @@ def get_gfn_forward_loss(loss_coeffs,
         loss_dict = None
 
     if return_exp:
-        return loss, states.detach(), log_pfs.detach(), log_pbs.detach(), log_r.detach(), log_fs.detach(), crystal_batch, loss_dict
+        return loss, crystal_batch.cpu().detach(), loss_dict
     else:
         return loss, loss_dict
+
+
+
+def get_gfn_backward_loss(loss_coeffs,
+                          samples,
+                          gfn,
+                          log_r,
+                          discretizer,
+                          condition=None,
+                          repeats=10,
+                          report_losses: bool = False):
+    if gfn.conditional_flow_model and any([
+        loss_coeffs.vg_lb > 0, loss_coeffs.vg_lme > 0
+    ]):
+        condition = condition.repeat(repeats, 1)
+        samples = samples.repeat(repeats, 1)
+        log_r = log_r.repeat(repeats)
+        conditional_repeats = True
+    else:
+        conditional_repeats = False
+
+    condition = condition.to(gfn.device)
+    states, log_pfs, log_pbs, log_flow = gfn.get_trajectory_bwd(
+        samples, discretizer, condition, return_gauss_params=False)
+
+    log_pf = log_pfs.sum(-1)
+    log_pb = log_pbs.sum(-1)
+
+    losses = []
+    """VarGrad - lower bound loss"""
+    if loss_coeffs.vg_lb > 0:
+        log_Z, vg_loss = vg_lb(gfn, log_pb, log_pf, log_r, loss_coeffs, repeats)
+
+        losses.append(vg_loss * loss_coeffs.vg_lb)
+
+        """VarGrad - log mean exp loss"""
+    elif loss_coeffs.vg_lme > 0:
+        log_Z, vg_loss = vg_lme(gfn, log_pb, log_pf, log_r, repeats)
+        losses.append(vg_loss * loss_coeffs.vg_lme)
+
+    if loss_coeffs.emp_z > 0:  # train the flow model to match the empirical log Z distribution
+        emp_z_loss = emp_Z(gfn, log_Z, log_flow, repeats)
+        losses.append(emp_z_loss * loss_coeffs.emp_z)
+    else:
+        emp_z_loss = None
+
+    """TB loss"""
+    if loss_coeffs.tb > 0:
+        emp_z_coeff, tb_loss = get_tb_loss(emp_z_loss, log_flow, log_pb, log_pf, log_r, loss_coeffs)
+        losses.append(tb_loss * loss_coeffs.tb * emp_z_coeff)
+
+    if loss_coeffs.mle > 0:
+        mle_loss = terminal_mle(
+            log_pf, log_pb,
+            repeats,
+            conditional_repeats,
+            estimator='exact' if conditional_repeats else 'bound',
+            dreg=True
+        )
+        losses.append(mle_loss * loss_coeffs.mle)
+
+    if loss_coeffs.loss_clip != -1:
+        combined_losses = soft_clip(torch.stack(losses), loss_coeffs.loss_clip).mean(dim=0)
+    else:
+        combined_losses = torch.stack(losses).mean(dim=0)
+    loss = combined_losses.mean()
+
+    if report_losses:
+        loss_dict = {}
+        if loss_coeffs.tb > 0:
+            loss_dict['tb'] = tb_loss.mean().detach()
+        if loss_coeffs.vg_lb > 0:
+            loss_dict['vg_lb'] = vg_loss.mean().detach()
+        if loss_coeffs.vg_lme > 0:
+            loss_dict['vg_lme'] = vg_loss.mean().detach()
+        if loss_coeffs.emp_z > 0:
+            loss_dict['emp_z'] = emp_z_loss.mean().detach()
+        if loss_coeffs.mle > 0:
+            loss_dict['mle'] = mle_loss.mean().detach()
+    else:
+        loss_dict = None
+
+    return loss, loss_dict
+
+
+def terminal_mle(
+        log_pf, log_pb,
+        reps: int = None,
+        do_repeats: bool = False,
+        estimator: str = "bound",  # "bound" (Jensen, eq. 28) or "exact" (IWAE, eq. 27)
+        dreg: bool = True,  # use detached responsibilities for "exact"
+):
+    """
+    Returns:
+        loss: scalar tensor
+        stats: dict with diagnostics
+    """
+    if not do_repeats:
+        repeats = 1
+    else:
+        repeats = 1 * reps
+
+    # reshape into [B, K] where B = number of distinct terminals (before repeating)
+    if log_pf.numel() % repeats != 0:
+        raise ValueError(f"log_pf size {log_pf.numel()} not divisible by repeats={repeats}")
+
+    B = log_pf.numel() // repeats
+    log_pf = log_pf.view(B, repeats)
+    log_pb = log_pb.view(B, repeats)
+
+    logw = log_pf - log_pb  # [B, K]  ; log importance weights for each path
+
+    if estimator == "bound":
+        # Eq. (28): E_{τ~Pb}[ log Pf(τ) - log Pb(τ|x) ]  (sample mean over paths)
+        # No importance weights needed; just average over the K samples.
+        loss = -logw.mean(dim=1)  # [B]
+
+        return loss
+
+    elif estimator == "exact":
+        assert do_repeats, "Exact MLE estimator requires minibatch repeats > 1"
+        # Eq. (27): log E_{τ~Pb}[ exp(logw) ] ≈ logsumexp(logw, dim=1) - log K
+        if dreg:
+            # DReG-style gradient: responsibilities detached to tame variance
+            with torch.no_grad():
+                alpha = torch.softmax(logw, dim=1)  # [B, K]
+            # Equivalent gradient to IWAE objective under DReG; stable
+            loss = -((alpha * logw).sum(dim=1))  # [B]
+        else:
+            # Plain IWAE objective (can be higher variance)
+            lse = torch.logsumexp(logw, dim=1)  # [B]
+            loss = -(lse - math.log(repeats))
+
+        return loss.repeat(repeats)
+
+    else:
+        raise ValueError("estimator must be 'bound' or 'exact'")
+
+
+def power_saturate(x, power):
+    return torch.sign(x) * (torch.abs(x) ** power)
 
 
 def get_tb_loss(emp_z_loss, log_flow, log_pb, log_pf, log_r, loss_coeffs):
@@ -322,155 +465,3 @@ def fwd_overlap_loss(gfn, loss_coeffs, repeats, states):
             agg='mean',
         )
     return overlap_loss
-
-
-def get_gfn_backward_loss(loss_coeffs,
-                          samples,
-                          gfn,
-                          log_r,
-                          discretizer,
-                          condition=None,
-                          repeats=10,
-                          return_exp=False,
-                          report_losses: bool = False):
-    if gfn.conditional_flow_model and any([
-        loss_coeffs.vg_lb > 0, loss_coeffs.vg_lme > 0
-    ]):
-        condition = condition.repeat(repeats, 1)
-        samples = samples.repeat(repeats, 1)
-        log_r = log_r.repeat(repeats)
-        conditional_repeats = True
-    else:
-        conditional_repeats = False
-
-    (states, log_pfs, log_pbs, log_fs,
-     means_f, logvars_f, means_b, logvars_b) = gfn.get_trajectory_bwd(
-        samples, discretizer, condition, return_gauss_params=True)
-
-    log_pf = log_pfs.sum(-1)
-    log_pb = log_pbs.sum(-1)
-    log_flow = log_fs[:, 0]
-
-    losses = []
-    if loss_coeffs.smoothed > 0:
-        smoothness_loss = normed_smoothness_loss(torch.stack([means_f, logvars_f, means_b, logvars_b])).mean(dim=0)
-        losses.append(smoothness_loss * loss_coeffs.smoothed)
-
-    """VarGrad - lower bound loss"""
-    if loss_coeffs.vg_lb > 0:
-        log_Z, vg_loss = vg_lb(gfn, log_pb, log_pf, log_r, loss_coeffs, repeats)
-
-        losses.append(vg_loss * loss_coeffs.vg_lb)
-
-        """VarGrad - log mean exp loss"""
-    elif loss_coeffs.vg_lme > 0:
-        log_Z, vg_loss = vg_lme(gfn, log_pb, log_pf, log_r, repeats)
-        losses.append(vg_loss * loss_coeffs.vg_lme)
-
-    if loss_coeffs.emp_z > 0:  # train the flow model to match the empirical log Z distribution
-        emp_z_loss = emp_Z(gfn, log_Z, log_flow, repeats)
-        losses.append(emp_z_loss * loss_coeffs.emp_z)
-    else:
-        emp_z_loss = None
-
-    """TB loss"""
-    if loss_coeffs.tb > 0:
-        emp_z_coeff, tb_loss = get_tb_loss(emp_z_loss, log_flow, log_pb, log_pf, log_r, loss_coeffs)
-        losses.append(tb_loss * loss_coeffs.tb * emp_z_coeff)
-
-    if loss_coeffs.mle > 0:
-        mle_loss = terminal_mle(
-            log_pf, log_pb,
-            repeats,
-            conditional_repeats,
-            estimator='exact' if conditional_repeats else 'bound',
-            dreg=True
-        )
-        #mle_loss = -power_saturate(log_pf, 0.7)
-        losses.append(mle_loss * loss_coeffs.mle)
-
-    if loss_coeffs.loss_clip != -1:
-        combined_losses = soft_clip(torch.stack(losses), loss_coeffs.loss_clip).mean(dim=0)
-    else:
-        combined_losses = torch.stack(losses).mean(dim=0)
-    loss = combined_losses.mean()
-
-    if report_losses:
-        loss_dict = {}
-        if loss_coeffs.smoothed > 0:
-            loss_dict['smoothed'] = smoothness_loss.mean().detach()
-        if loss_coeffs.tb > 0:
-            loss_dict['tb'] = tb_loss.mean().detach()
-        if loss_coeffs.vg_lb > 0:
-            loss_dict['vg_lb'] = vg_loss.mean().detach()
-        if loss_coeffs.vg_lme > 0:
-            loss_dict['vg_lme'] = vg_loss.mean().detach()
-        if loss_coeffs.emp_z > 0:
-            loss_dict['emp_z'] = emp_z_loss.mean().detach()
-        if loss_coeffs.mle > 0:
-            loss_dict['mle'] = mle_loss.mean().detach()
-    else:
-        loss_dict = None
-
-    if return_exp:
-        return loss, states.detach(), log_pfs.detach(), log_pbs.detach(), log_r.detach(), log_fs.detach(), loss_dict
-    else:
-        return loss, loss_dict
-
-
-def terminal_mle(
-        log_pf, log_pb,
-        reps: int = None,
-        do_repeats: bool = False,
-        estimator: str = "bound",  # "bound" (Jensen, eq. 28) or "exact" (IWAE, eq. 27)
-        dreg: bool = True,  # use detached responsibilities for "exact"
-):
-    """
-    Returns:
-        loss: scalar tensor
-        stats: dict with diagnostics
-    """
-    if not do_repeats:
-        repeats = 1
-    else:
-        repeats = 1 * reps
-
-    # reshape into [B, K] where B = number of distinct terminals (before repeating)
-    if log_pf.numel() % repeats != 0:
-        raise ValueError(f"log_pf size {log_pf.numel()} not divisible by repeats={repeats}")
-
-    B = log_pf.numel() // repeats
-    log_pf = log_pf.view(B, repeats)
-    log_pb = log_pb.view(B, repeats)
-
-    logw = log_pf - log_pb  # [B, K]  ; log importance weights for each path
-
-    if estimator == "bound":
-        # Eq. (28): E_{τ~Pb}[ log Pf(τ) - log Pb(τ|x) ]  (sample mean over paths)
-        # No importance weights needed; just average over the K samples.
-        loss = -logw.mean(dim=1)  # [B]
-
-        return loss
-
-    elif estimator == "exact":
-        assert do_repeats, "Exact MLE estimator requires minibatch repeats > 1"
-        # Eq. (27): log E_{τ~Pb}[ exp(logw) ] ≈ logsumexp(logw, dim=1) - log K
-        if dreg:
-            # DReG-style gradient: responsibilities detached to tame variance
-            with torch.no_grad():
-                alpha = torch.softmax(logw, dim=1)  # [B, K]
-            # Equivalent gradient to IWAE objective under DReG; stable
-            loss = -((alpha * logw).sum(dim=1))  # [B]
-        else:
-            # Plain IWAE objective (can be higher variance)
-            lse = torch.logsumexp(logw, dim=1)  # [B]
-            loss = -(lse - math.log(repeats))
-
-        return loss.repeat(repeats)
-
-    else:
-        raise ValueError("estimator must be 'bound' or 'exact'")
-
-
-def power_saturate(x, power):
-    return torch.sign(x) * (torch.abs(x) ** power)

energy_sampling/models/gfn.py
@@ -110,7 +110,7 @@ class GFN(nn.Module):
         ts = discretizer(batch_size).to(self.device)
         trajectory_length = ts.shape[1] - 1
 
-        logf, logpb, logpf, states, means_f, logvars_f, means_b, logvars_b = self.init_traj_tensors(batch_size,
+        logpb, logpf, states, means_f, logvars_f, means_b, logvars_b = self.init_traj_tensors(batch_size,
                                                                                                     trajectory_length)
 
         initial_state.requires_grad_(not detach_traj)
@@ -119,7 +119,7 @@ class GFN(nn.Module):
 
         condition_embedding = self.conditions_embedding_model(condition)
 
-        logf[:, 0] = self.flow_model(condition_embedding).squeeze(-1).squeeze(-1)
+        logf = self.flow_model(condition_embedding).flatten()
 
         for i in range(trajectory_length):
             dts = ts[:, i + 1] - ts[:, i]
@@ -131,8 +131,8 @@ class GFN(nn.Module):
 
             noise = ((next_state - current_state) - dts.unsqueeze(1) * pf_mean) / (
                     dts.sqrt().unsqueeze(1) * (pflogvars / 2).exp())
-            logpf[:, i] = -0.5 * (noise ** 2 + logtwopi + dts.log().unsqueeze(1) + pflogvars).sum(1)
-
+            #logpf[:, i] = -0.5 * (noise ** 2 + logtwopi + dts.log().unsqueeze(1) + pflogvars).sum(1)
+            logpf.append(-0.5 * (noise ** 2 + logtwopi + dts.log().unsqueeze(1) + pflogvars).sum(1))
             back_mean_correction, back_var_correction = self.fwd_get_back_correction(
                 condition_embedding, i, next_state, ts)
             back_mean = (next_state - next_state * (dts / ts[:, i + 1]).unsqueeze(1) * back_mean_correction)
@@ -142,7 +142,8 @@ class GFN(nn.Module):
                 var = (back_var_correction + np.log(self.pf_std_per_traj) * 2.0).clip(min=-self.var_clip, max=self.var_clip).exp()
                 back_var = var * (dts * ts[:, i] / ts[:, i + 1]).unsqueeze(1)
                 noise_backward = (current_state - back_mean) / back_var.sqrt()
-                logpb[:, i] = -0.5 * (noise_backward ** 2 + logtwopi + back_var.log()).sum(1)
+                #logpb[:, i] = -0.5 * (noise_backward ** 2 + logtwopi + back_var.log()).sum(1)
+                logpb.append(-0.5 * (noise_backward ** 2 + logtwopi + back_var.log()).sum(1))
             else:  # instead set this as a constant the model will have to learn around
                 back_var = torch.ones_like(back_mean) * 1e-3 * dts.unsqueeze(1)
 
@@ -150,17 +151,19 @@ class GFN(nn.Module):
             states[:, i + 1] = current_state
 
             if return_gauss_params:
-                means_b[:, i, :] = back_mean - current_state
-                logvars_b[:, i, :] = (back_var / dts[:, None]).log()
-                means_f[:, i, :] = pf_mean * dts[:, None]
-                logvars_f[:, i, :] = pflogvars
+                means_b[:, i, :] = (back_mean - current_state).detach()
+                logvars_b[:, i, :] = (back_var / dts[:, None]).log().detach()
+                means_f[:, i, :] = (pf_mean * dts[:, None]).detach()
+                logvars_f[:, i, :] = pflogvars.detach()
 
+        logpfs = torch.stack(logpf).T
+        logpbs = torch.stack(logpb).T
         if return_gauss_params:
-            return (states, logpf, logpb, logf,
+            return (states, logpfs, logpbs, logf,
                     means_f.mean(-1), logvars_f.mean(-1),
                     means_b.mean(-1), logvars_b.mean(-1))
         else:
-            return states, logpf, logpb, logf
+            return states, logpfs, logpbs, logf
 
     def get_trajectory_bwd(self, terminal_state, discretizer, condition,
                            return_gauss_params: bool = False, detach_traj: bool = False):
@@ -169,15 +172,15 @@ class GFN(nn.Module):
         ts = discretizer(batch_size).to(self.device)
         trajectory_length = ts.shape[1] - 1
 
-        logf, logpb, logpf, states, means_f, logvars_f, means_b, logvars_b = self.init_traj_tensors(batch_size,
+        logpb, logpf, states, means_f, logvars_f, means_b, logvars_b = self.init_traj_tensors(batch_size,
                                                                                                     trajectory_length)
 
-        states[:, -1] = terminal_state.detach().clone()
-        current_state = terminal_state.detach().clone()
+        states[:, -1] = terminal_state.detach()
+        current_state = terminal_state.detach()
         condition_embedding = self.conditions_embedding_model(condition)
 
-        flow = self.flow_model(condition_embedding).squeeze(-1).squeeze(-1)
-        logf[:, 0] = flow
+        logf = self.flow_model(condition_embedding).flatten()
+        log_var_base = 2.0 * math.log(float(self.pf_std_per_traj))
 
         for i in range(trajectory_length):
             dts = ts[:, trajectory_length - i] - ts[:, trajectory_length - i - 1]
@@ -192,11 +195,12 @@ class GFN(nn.Module):
                 # back_var = ((self.pf_std_per_traj ** 2) *
                 #             (dts * ts[:, trajectory_length - i - 1] / ts[:, trajectory_length - i]).unsqueeze(
                 #                 1) * back_var_correction)
-                var = (back_var_correction + np.log(self.pf_std_per_traj) * 2.0).clip(min=-self.var_clip, max=self.var_clip).exp()
+                var = (back_var_correction + log_var_base).clip(min=-self.var_clip, max=self.var_clip).exp()
                 back_var = var * (dts * ts[:, trajectory_length - i - 1] / ts[:, trajectory_length - i]).unsqueeze(1)
                 prev_state = self.bwd_propagate(back_mean, back_var, current_state, detach_traj)
                 noise_backward = (prev_state - back_mean) / back_var.sqrt()
-                logpb[:, trajectory_length - i - 1] = -0.5 * (noise_backward ** 2 + logtwopi + back_var.log()).sum(1)
+                #logpb[:, trajectory_length - i - 1] = -0.5 * (noise_backward ** 2 + logtwopi + back_var.log()).sum(1)
+                logpb.append(-0.5 * (noise_backward ** 2 + logtwopi + back_var.log()).sum(1))
             else:
                 # send it identically to the source state (0)
                 prev_state = torch.zeros_like(current_state)
@@ -208,25 +212,30 @@ class GFN(nn.Module):
 
             noise = ((current_state - prev_state) - dts.unsqueeze(1) * pf_mean) / (
                     dts.sqrt().unsqueeze(1) * (pflogvars / 2).exp())
-            logpf[:, trajectory_length - i - 1] = -0.5 * (
+            # logpf[:, trajectory_length - i - 1] = -0.5 * (
+            #         noise ** 2 + logtwopi + dts.log().unsqueeze(1) + pflogvars).sum(
+            #     1)
+            logpf.append(-0.5 * (
                     noise ** 2 + logtwopi + dts.log().unsqueeze(1) + pflogvars).sum(
-                1)
+                1))
 
             if return_gauss_params:
-                means_b[:, i, :] = back_mean - current_state
-                logvars_b[:, i, :] = (back_var / dts[:, None]).log()
-                means_f[:, i, :] = pf_mean * dts[:, None]
-                logvars_f[:, i, :] = pflogvars
+                means_b[:, i, :] = (back_mean - current_state).detach()
+                logvars_b[:, i, :] = (back_var / dts[:, None]).log().detach()
+                means_f[:, i, :] = (pf_mean * dts[:, None]).detach()
+                logvars_f[:, i, :] = pflogvars.detach()
 
             current_state = prev_state
-            states[:, trajectory_length - i - 1] = current_state
+            states[:, trajectory_length - i - 1] = current_state.detach()
 
+        logpfs = torch.stack(logpf).T
+        logpbs = torch.stack(logpb).T
         if return_gauss_params:
-            return (states, logpf, logpb, logf,
+            return (states, logpfs, logpbs, logf,
                     means_f.mean(-1), logvars_f.mean(-1),
                     means_b.mean(-1), logvars_b.mean(-1))
         else:
-            return states, logpf, logpb, logf
+            return states, logpfs, logpbs, logf
 
     def fwd_get_back_correction(self, condition_embedding, i, next_state, ts):
         if self.learn_pb:
@@ -303,13 +312,13 @@ class GFN(nn.Module):
         return back_mean_correction, back_additive_logvar
 
     def init_traj_tensors(self, batch_size, trajectory_length):
-        logpf = torch.zeros((batch_size, trajectory_length), device=self.device)
-        logpb = torch.zeros((batch_size, trajectory_length), device=self.device)
-        logf = torch.zeros((batch_size, trajectory_length + 1), device=self.device)
+        logpf = []#torch.zeros((batch_size, trajectory_length), device=self.device)
+        logpb = []#torch.zeros((batch_size, trajectory_length), device=self.device)
+        #logf = torch.zeros((batch_size, trajectory_length + 1), device=self.device)
         states = torch.zeros((batch_size, trajectory_length + 1, self.dim), device=self.device)
         means_f = torch.zeros((batch_size, trajectory_length, self.dim), device=self.device)
         logvars_f = torch.zeros((batch_size, trajectory_length, self.dim), device=self.device)
         means_b = torch.zeros((batch_size, trajectory_length, self.dim), device=self.device)
         logvars_b = torch.zeros((batch_size, trajectory_length, self.dim), device=self.device)
 
-        return logf, logpb, logpf, states, means_f, logvars_f, means_b, logvars_b
+        return logpb, logpf, states, means_f, logvars_f, means_b, logvars_b

energy_sampling/utils.py
@@ -297,6 +297,7 @@ def triangle_schedule(it, init, maxval, minval, on, off):
         return minval
 
 
+@torch.inference_mode()
 def featurize_dataset(dataset, device, ellipsoid_scale, lj_repulsion):
     batch_size = 500
     loader = DataLoader(
@@ -332,10 +333,10 @@ def featurize_dataset(dataset, device, ellipsoid_scale, lj_repulsion):
 
             niggli_overlap = cluster_batch.compute_niggli_overlap()
 
-            overlaps.extend(normed_ellipsoid_overlap.cpu().detach().numpy())
-            silus.extend(silu_energy.cpu().detach().numpy())
-            ljs.extend(lj_energy.cpu().detach().numpy())
-            niggli_overlaps.extend(niggli_overlap)
+            overlaps.extend(normed_ellipsoid_overlap.cpu().detach())
+            silus.extend(silu_energy.cpu().detach())
+            ljs.extend(lj_energy.cpu().detach())
+            niggli_overlaps.extend(niggli_overlap.cpu().detach())
 
         overlaps = torch.tensor(overlaps)
         silus = torch.tensor(silus)
@@ -349,10 +350,11 @@ def featurize_dataset(dataset, device, ellipsoid_scale, lj_repulsion):
 
     # exclude negative niggli overlaps
     dataset = [elem for elem in dataset if elem.niggli_overlap >= 0]
+    [elem.box_analysis() for elem in dataset]
 
     return dataset
 
-
+@torch.inference_mode()
 def embed_dataset(dataset, autoencoder_path=None, device=None, encoder=None, embedding_type='autoencoder'):
     batch_size = 500
     loader = DataLoader(
@@ -382,7 +384,7 @@ def embed_dataset(dataset, autoencoder_path=None, device=None, encoder=None, emb
                     crystal_batch.num_graphs,
                     crystal_batch.num_atoms,
                 )
-                embeddings.append(v_embedding_i * s_embedding_i[:, :, None].clone().cpu())
+                embeddings.append((v_embedding_i * s_embedding_i[:, :, None]).cpu())
 
             del crystal_batch
 