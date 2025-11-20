:> Uma integration (partial) :> automated reward scaling :> some simplification :> protonated crystals fixes :> set several crystal ops to heavy atoms only for consistency

energy_sampling/buffer.py
@@ -46,23 +46,22 @@ class CrystalReplayBuffer:
         self.max_z_prime = max_z_prime
         self.buffer_dist_cutoff = buffer_dist_cutoff
 
+    @torch.no_grad()
     def add(self,
             data_list=None,
             data_batch=None,
             max_z_prime: int = 1):
-        with torch.no_grad():
-            if self.dataset is None:
-                self.init_fresh_dataset(data_list, max_z_prime)
+        if self.dataset is None:
+            self.init_fresh_dataset(data_list, max_z_prime)
+        else:
+            self.add_samples_to_dataset(data_batch, data_list, max_z_prime)
 
-            else:
-                self.add_samples_to_dataset(data_batch, data_list, max_z_prime)
+        if len(self) > self.buffer_size:  # pare down buffer
+            self.truncate_buffer()
 
-            if len(self) > self.buffer_size:  # pare down buffer
-                self.truncate_buffer()
+        assert len(self.dataset) == len(self.x_list) == len(self.rewards_list)
 
-            assert len(self.dataset) == len(self.x_list) == len(self.rewards_list)
-
-            self.init_loader()
+        self.init_loader()
 
     def add_samples_to_dataset(self, data_batch, data_list, max_z_prime):
         # batch samples
@@ -93,15 +92,18 @@ class CrystalReplayBuffer:
 
     def init_fresh_dataset(self, data_list, max_z_prime):
         self.dataset = list(data_list)  # I think this is memory safe and faster #copy.deepcopy(data_list)
-        for elem in self.dataset:
+        for elem in self.dataset:  # have to do this now because collation is a mess
             del elem.fingerprint, elem.smiles, elem.mol_ind, elem.identifier, (
                 elem.aunit_batch), elem.skip_box_analysis, elem.cocrystal, elem.symmetry_operators
 
         dataset_batch = collate_data_list(self.dataset, max_z_prime=max_z_prime)
         x_tensor = dataset_batch.latent_params()
-        scores = self.energy_function.prebuilt_sample_to_reward(dataset_batch, temperature=torch.ones(len(self)))
+        rewards = self.energy_function.prebuilt_sample_to_reward(dataset_batch, temperature=torch.ones(len(self)))
+        if self.energy_function.reward_range is not None:
+            self.energy_function.set_reward_clip(rewards)
+
         self.x_list = [x_tensor[i] for i in range(x_tensor.shape[0])]
-        self.rewards_list = list(scores.flatten().cpu().detach().numpy())
+        self.rewards_list = list(rewards.flatten().cpu().detach().numpy())
         self.original_dataset_inds = list(np.arange(len(self.dataset)))
         self.sg_list = list(dataset_batch.sg_ind.cpu())
 

energy_sampling/energies/molecular_crystal.py
@@ -10,6 +10,7 @@ from mxtaltools.constants.space_group_feature_tensor import SG_FEATURE_TENSOR
 from mxtaltools.constants.space_group_info import SYM_OPS
 from mxtaltools.dataset_utils.data_classes import MolCrystalData
 from mxtaltools.dataset_utils.utils import collate_data_list
+from mxtaltools.mlip_interfaces.uma_utils import init_uma_crystal_predictor
 from .base_set import BaseSet
 
 
@@ -40,12 +41,10 @@ class MolecularCrystal(BaseSet):
                  energy_function: str,
                  max_temperature: float = 10,
                  min_temperature: float = 0.01,
-                 lj_turnover_pot: float = 10.0,
                  density_coeff: float = 0,
                  temperature_scaling_factor: float = 1,
                  temperature: float = 1.0,
                  temperature_conditioning: bool = False,
-                 energy_clip: float = 100,
                  lj_coeff: float = 1.0,
                  molecule_conditioning: bool = False,
                  sg_conditioning: bool = False,
@@ -55,13 +54,14 @@ class MolecularCrystal(BaseSet):
                  niggli_coeff: float = 1.0,
                  max_z_prime: int = 1,
                  z_primes: Tuple[int] = (1,),
+                 uma_path: Optional[str] = None,
+                 reward_range: float = None,
                  ):
 
         super(MolecularCrystal, self).__init__()
         self.device = device
         self.data_ndim = 6 + 6 * max_z_prime
         self.energy_function = energy_function
-        self.energy_clip = energy_clip
         self.SG_FEATURE_TENSOR = SG_FEATURE_TENSOR.clone()  # store space group information
 
         self.density_coeff = density_coeff
@@ -69,7 +69,6 @@ class MolecularCrystal(BaseSet):
         self.min_temperature = min_temperature
         self.temperature_scaling_factor = temperature_scaling_factor
         self.temperature_conditioning = temperature_conditioning
-        self.lj_turnover_pot = lj_turnover_pot  # energy above which to soften intermolecular repulsion
         self.lj_coeff = lj_coeff
         self.bounding_coeff = bounding_coeff
         self.niggli_coeff = niggli_coeff
@@ -79,14 +78,30 @@ class MolecularCrystal(BaseSet):
         self.max_z_prime = max_z_prime
         self.z_primes = z_primes
         self.zp_conditioning = zp_conditioning
+        self.reward_range = reward_range
+        if self.energy_function == 'uma':
+            self.uma_predictor = init_uma_crystal_predictor(uma_path, device=self.device)
 
         self.temperature = temperature  # for static temperature work
+        self.energy_clip = 0
 
         self.batch = collate_data_list([MolCrystalData(max_z_prime=max_z_prime)], max_z_prime=max_z_prime)
 
         self.sg_cache = {}
         for sg in range(1, 230):
             self.sg_cache[sg] = np.stack(SYM_OPS[int(sg)])
+
+    def set_reward_clip(self, dataset_rewards):
+        """
+        We want to restrain the range of allowable rewards, by log-clipping the log reward below a certain threshold.
+        NOTE this would have to be re-done dynamically if the conditioning evolves
+        :param dataset_rewards:
+        :return:
+        """
+        max_reward = max(dataset_rewards)
+        reward_range = self.reward_range
+        min_allowed_reward = max_reward - reward_range
+        self.energy_clip = - min_allowed_reward * self.temperature  # convert the minimum allowed reward to a clip on the energy
 
     def instantiate_crystals(self, x, mol_batch):
         crystal_batch = self.init_blank_crystal_batch(mol_batch)
@@ -97,19 +112,16 @@ class MolecularCrystal(BaseSet):
     def analyze_crystal_batch(self, x, mol_batch, return_batch=False):  # x is gfn_outputs
         crystal_batch = self.instantiate_crystals(x, mol_batch)
 
-        if self.energy_function not in ['lj', 'qlj', 'silu']:
+        if self.energy_function not in ['lj', 'qlj', 'silu','uma']:
             lj_energy = torch.zeros(crystal_batch.num_graphs, device=self.device)
             qlj_energy = torch.zeros_like(lj_energy)
             normed_lj_energy = torch.zeros_like(lj_energy)
             silu_energy = torch.zeros_like(lj_energy)
             niggli_overlap = torch.zeros_like(lj_energy)
+            uma_energy = torch.zeros_like(lj_energy)
         else:
-            if self.energy_function in ['lj', 'qlj']:
-                cutoff = 10
-            elif self.energy_function == 'silu':
-                cutoff = 6
-            else:
-                assert False
+            cutoff = 10
+
             out = crystal_batch.analyze(['lj', 'qlj', 'silu', 'niggli'],
                                         cutoff=cutoff,
                                         supercell_size=5,
@@ -119,12 +131,18 @@ class MolecularCrystal(BaseSet):
             normed_lj_energy = log_rescale_positive(lj_energy, 0)
             silu_energy = out['silu']
             niggli_overlap = out['niggli']
+            if self.energy_function == 'uma':
+                uma_energy = crystal_batch.compute_crystal_uma(
+                    predictor=self.uma_predictor, std_orientation=False) * 96.485  # output in kJ/mol (of unit cells)
+            else:
+                uma_energy = torch.zeros_like(lj_energy)
 
         crystal_batch.add_graph_attr(silu_energy, 'silu_pot')
         crystal_batch.add_graph_attr(lj_energy, 'lj_pot')
         crystal_batch.add_graph_attr(qlj_energy, 'qlj_pot')
         crystal_batch.add_graph_attr(niggli_overlap, 'niggli_overlap')
         crystal_batch.add_graph_attr(normed_lj_energy, 'scaled_lj_pot')
+        crystal_batch.add_graph_attr(uma_energy, 'uma_pot')
 
         crystal_energy, ens_dict = self.generator_energy(crystal_batch, raw_latents=x)
 
@@ -153,34 +171,27 @@ class MolecularCrystal(BaseSet):
             bounding_energy = torch.zeros_like(latents[:, 0])
 
         if self.max_z_prime > 1:
-            # penalize the model for placing asymmetric units out of the canonical order (closest -> furthest from origin)
-            per_aunit_centroids = crystal_batch.aunit_centroid.reshape(crystal_batch.num_graphs,
-                                                                       crystal_batch.max_z_prime, 3)
-            idx = torch.arange(crystal_batch.max_z_prime, device=crystal_batch.device)[None, ...]
-            mask = (idx >= (crystal_batch.z_prime[..., None]))[..., None].expand(-1, -1, 3)
-            per_aunit_centroids[mask] = 1  # this will put lower Z' options always at the end
-            origin_dists = per_aunit_centroids.norm(dim=2)
+            bounding_energy = self.compute_zp_order_penalty(bounding_energy, crystal_batch)
 
-            overlaps = -origin_dists.diff(dim=1)
-            zp_ordering_energy = F.relu(overlaps).mean(dim=-1) ** 2
-            bounding_energy = bounding_energy + zp_ordering_energy
-
-        if self.energy_function in ['lj', 'qlj', 'silu']:
+        if self.energy_function in ['lj', 'qlj', 'silu','uma']:
             density_energy = density_penalty(crystal_batch.packing_coeff)
             if self.energy_function == 'lj':
-                lj_energy = log_rescale_positive(crystal_batch.lj_pot, self.lj_turnover_pot) / crystal_batch.num_atoms
+                mol_energy = crystal_batch.lj_pot / crystal_batch.num_atoms
             elif self.energy_function == 'qlj':
-                lj_energy = soften_high(crystal_batch.qlj_pot, self.lj_turnover_pot,
-                                        coeff=0.25) / crystal_batch.num_atoms
+                mol_energy = crystal_batch.qlj_pot / crystal_batch.num_atoms
             elif self.energy_function == 'silu':
-                lj_energy = soften_high(crystal_batch.silu_pot, self.lj_turnover_pot,
-                                        coeff=0.25) / crystal_batch.num_atoms
+                mol_energy = crystal_batch.silu_pot / crystal_batch.num_atoms
+            elif self.energy_function == 'uma':
+                gas_pot = -38303.8359  # temporary hardcode - nicotinamide unit cell gas energy under uma esen-s
+                #mol_energy = (crystal_batch.uma_pot / crystal_batch.sym_mult - crystal_batch.uma_gas_pot/crystal_batch.sym_mult) / crystal_batch.num_atoms  # gas pots are messed up
+                mol_energy = (crystal_batch.uma_pot / crystal_batch.sym_mult - gas_pot/crystal_batch.sym_mult[0]) / crystal_batch.num_atoms  # TODO REPLACE THIS LINE EVENTUALLY
             else:
                 assert False
+
             niggli_energy = F.relu(-crystal_batch.niggli_overlap) ** 2  # punish negative overlaps
 
             ens_dict['niggli_energy'] = niggli_energy
-            ens_dict['lj_energy'] = lj_energy
+            ens_dict['mol_energy'] = mol_energy
             ens_dict['density_energy'] = density_energy
             ens_dict['bounding_energy'] = bounding_energy
         else:
@@ -198,15 +209,27 @@ class MolecularCrystal(BaseSet):
         elif self.energy_function == 'crystal_multiharmonic':
             crystal_energy = self.crystal_multiharmonic_en(crystal_batch, latents)
 
-        elif self.energy_function in ['lj', 'qlj', 'silu']:
-            crystal_energy = self.lj_coeff * lj_energy + self.density_coeff * density_energy
+        elif self.energy_function in ['lj', 'qlj', 'silu', 'uma']:
+            crystal_energy = self.lj_coeff * mol_energy + self.density_coeff * density_energy
 
         else:
             assert False, f'{self.energy_function} not implemented'
 
         total_energy = crystal_energy + bounding_energy * self.bounding_coeff + niggli_energy * self.niggli_coeff
-        # return soft_clip(soften_high(total_energy, self.energy_clip / 2, coeff=0.7), self.energy_clip), ens_dict
         return log_rescale_positive(total_energy, self.energy_clip), ens_dict
+
+    def compute_zp_order_penalty(self, bounding_energy, crystal_batch):
+        # penalize the model for placing asymmetric units out of the canonical order (closest -> furthest from origin)
+        per_aunit_centroids = crystal_batch.aunit_centroid.reshape(crystal_batch.num_graphs,
+                                                                   crystal_batch.max_z_prime, 3)
+        idx = torch.arange(crystal_batch.max_z_prime, device=crystal_batch.device)[None, ...]
+        mask = (idx >= (crystal_batch.z_prime[..., None]))[..., None].expand(-1, -1, 3)
+        per_aunit_centroids[mask] = 1  # this will put lower Z' options always at the end
+        origin_dists = per_aunit_centroids.norm(dim=2)
+        overlaps = -origin_dists.diff(dim=1)
+        zp_ordering_energy = F.relu(overlaps).mean(dim=-1) ** 2
+        bounding_energy = bounding_energy + zp_ordering_energy
+        return bounding_energy
 
     def crystal_multiharmonic_en(self, crystal_batch, latents):
         if not hasattr(self, 'modes'):
@@ -344,6 +367,7 @@ class MolecularCrystal(BaseSet):
             'density': zeros1,
             'z_prime': ones1,
             'is_well_defined': trues1,
+            'uma_lattice_pot': zeros1,
         }
         setattr(crystal_batch, '_num_graphs', mol_batch.num_graphs)
         setattr(crystal_batch, 'device', mol_batch.device)

energy_sampling/eval/evaluations.py
@@ -326,7 +326,7 @@ def boltzmann_fig(log_r):
 
     # === Boltzmann fit in linear space
     boltzmann_y = np.exp(-beta_est * x_kde)
-    boltzmann_y /= np.trapz(boltzmann_y, x_kde)
+    boltzmann_y /= (np.trapz(boltzmann_y, x_kde) + 1e-6)
     log_fit = slope * bin_centers + intercept
 
     # === Create subplots

energy_sampling/eval/paper1_results/analysis.py
@@ -5,19 +5,19 @@ from umap import UMAP
 
 from energy_sampling.eval.paper1_results.utils import cluster_dendro_fig, marginal_cluster_1d, coupling_ratio, \
     correlate_mask, top_joint_correlates, latent_dendro_fig, plot_marginals, get_highp_correlations, \
-    compute_dim_weights, estimate_logp_with_convergence, sample_from_gfn, analyze_samples
+    compute_dim_weights, estimate_logp_with_convergence, sample_from_gfn, analyze_samples, cluster_hdbscan_to_df
 from energy_sampling.models import GFN
 from mxtaltools.common.utils import log_rescale_positive
 from mxtaltools.dataset_utils.utils import collate_data_list
 
 if __name__ == '__main__':
     device = 'cuda'
-    num_samples = 20000
+    num_samples = 2000
     batch_size = 500
     n_steps = 50  # critical to get this right!
 
-    model_path = r"D:\crystal_datasets\nov_nic_6_0_model_eval.pt"
-    config_path = r"D:\crystal_datasets\nov_nic_6_0_model_config.npy"
+    model_path = r"D:\crystal_datasets\nov_nic_9_2_model_eval.pt"
+    config_path = r"D:\crystal_datasets\nov_nic_9_2_model_config.npy"
     molecule_path = r'D:/crystal_datasets/nicotinamide.pt'
     dataset_path = r'D:/crystal_datasets/opt_outputs/nicotinamide_sg_1_2.pt'
 
@@ -36,7 +36,6 @@ if __name__ == '__main__':
     sample_latents = sample_from_gfn(num_samples, max_z_prime, device, n_steps, batch_size, gfn_model)
     samples = analyze_samples(sample_latents, molecule * len(sample_latents), max_z_prime, device, batch_size)
     sample_batch = collate_data_list(samples, max_z_prime=max_z_prime)
-
 
     """Analyses"""
     "Standard visualizations"
@@ -61,7 +60,8 @@ if __name__ == '__main__':
         correlate_mask(marginal_labels, row.dims, row.clusters)
         for _, row in corr_df[corr_df.order == 2].iterrows()
     ]
-    sample_batch.plot_batch_cell_params(space='latent', aux_dists=[sample_batch.latent_params()[m] for m in masks[:10]])
+    sample_batch.plot_batch_cell_params(space='latent',
+                                        aux_dists=[sample_batch.latent_params()[m] for m in masks[:10] if sum(m) > 1])
 
     "High coupling n-dimensional correlation clusters for n=n_dims"
     plot_marginals(sample_latents, labels=marginal_labels, clusters_per_dim=clusters_per_dim)
@@ -92,22 +92,6 @@ if __name__ == '__main__':
     "Hierarchical joint probabilities"
     # df = hierarchical_joint_df(marginal_labels, max_order=3, cutoff=0.005)
 
-    "Dimension Reduction"
-    umap_model = UMAP(n_components=2, n_neighbors=100, min_dist=0.01)
-    sample_embedding = umap_model.fit_transform(sample_latents[low_en_bools])
-    masks = [correlate_mask(marginal_labels, top_df.loc[ind, "dims"], top_df.loc[ind, "clusters"])[low_en_bools] for ind
-             in
-             range(20)]
-    # masks = [
-    #     correlate_mask(marginal_labels, row.dims, row.clusters)[low_en_bools]
-    #     for _, row in corr_df[corr_df.order == 2].iloc[:20].iterrows()
-    # ]
-    fig = go.Figure()
-    fig.add_scatter(x=sample_embedding[:, 0], y=sample_embedding[:, 1], mode='markers', opacity=0.25, showlegend=False)
-    for ind, m in enumerate(masks):
-        fig.add_scatter(x=sample_embedding[m, 0], y=sample_embedding[m, 1], mode='markers', opacity=1.0, showlegend=False, marker_color=ind)
-    fig.show()
-
     "Top Cluster Analysis"
     masks = [correlate_mask(marginal_labels, top_df.loc[ind, "dims"], top_df.loc[ind, "clusters"]) for ind
              in
@@ -115,59 +99,30 @@ if __name__ == '__main__':
     sample_batch.plot_batch_cell_params(space='latent', aux_dists=[sample_batch.latent_params()[m] for m in masks[:10]])
     top_df.p / top_df.p.sum()
 
-    import torch
-    import numpy as np
-    import pandas as pd
-    import hdbscan
-
-
-    def cluster_hdbscan_to_df(X: torch.Tensor,
-                              lj_pot: torch.Tensor,
-                              min_cluster_size: int = 10):
-        """
-        Run HDBSCAN on samples X [n, d] and summarize results in a DataFrame.
-
-        Parameters
-        ----------
-        X : torch.Tensor [n, d]
-            Sample coordinates.
-        lj_pot : torch.Tensor [n]
-            Per-sample energies.
-        min_cluster_size : int
-            Minimum cluster size for HDBSCAN.
-
-        Returns
-        -------
-        pd.DataFrame with columns ['cluster_id', 'n', 'p', 'mean_en'].
-        """
-
-        # --- run clustering on CPU numpy array ---
-        clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size)
-        labels = clusterer.fit_predict(X.cpu().numpy())
-
-        # cluster probabilities (membership strengths)
-
-        # --- collect cluster masks and statistics ---
-        df_records = []
-        unique_labels = np.unique(labels)
-        for cid in unique_labels:
-            mask = labels == cid
-            n_points = int(mask.sum())
-            p_mean = n_points / len(X)
-
-            if cid == -1:  # noise cluster
-                mean_en = float(torch.tensor(log_rescale_positive(lj_pot[mask])).mean().cpu())
-            else:
-                mean_en = float(log_rescale_positive(lj_pot[mask]).mean().cpu())
-
-            df_records.append(dict(cluster_id=cid, n=n_points, p=p_mean, mean_en=mean_en))
-
-        top_df = pd.DataFrame(df_records, columns=['cluster_id', 'n', 'p', 'mean_en'])
-        return top_df, labels
-
-
     clust_df, clust_labels = cluster_hdbscan_to_df(sample_batch.latent_params(), sample_batch.lj_pot)
 
-
     clust_df = clust_df.sort_values('p', ascending=False)
+
+    "Dimension Reduction"
+    umap_model = UMAP(n_components=2, n_neighbors=100, min_dist=0.01)
+    sample_embedding = umap_model.fit_transform(sample_latents)#[low_en_bools])
+    # masks = [correlate_mask(marginal_labels, top_df.loc[ind, "dims"], top_df.loc[ind, "clusters"])[low_en_bools] for ind
+    #          in
+    #          range(20)]
+    # masks = [correlate_mask(marginal_labels, top_df.loc[ind, "dims"], top_df.loc[ind, "clusters"]) for ind
+    #          in
+    #          range(20)]
+    # masks = [
+    #     correlate_mask(marginal_labels, row.dims, row.clusters)[low_en_bools]
+    #     for _, row in corr_df[corr_df.order == 2].iloc[:20].iterrows()
+    # ]
+    masks = [clust_labels == ind for ind in np.unique(clust_labels)]
+    fig = go.Figure()
+    fig.add_scatter(x=sample_embedding[:, 0], y=sample_embedding[:, 1], mode='markers', opacity=0.25, showlegend=False)
+    maskes = [clust_labels == ind for ind in np.unique(clust_labels)]
+    for ind, m in enumerate(masks):
+        fig.add_scatter(x=sample_embedding[m, 0], y=sample_embedding[m, 1], mode='markers', opacity=1.0,
+                        showlegend=False, marker_color=ind)
+    fig.show()
+
     end = 1

energy_sampling/eval/paper1_results/utils.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+import hdbscan
 import numpy as np
 import pandas as pd
 import torch
@@ -13,6 +14,7 @@ from sklearn.cluster import estimate_bandwidth, MeanShift
 from tqdm import tqdm
 
 from energy_sampling.utils import uniform_discretizer, get_gfn_init_state
+from mxtaltools.common.utils import log_rescale_positive
 from mxtaltools.dataset_utils.utils import collate_data_list
 
 
@@ -26,7 +28,7 @@ def cluster_1d(X):
 
 def plot_marginals(latents, labels, clusters_per_dim):
     fig = make_subplots(rows=4, cols=3)
-    colors = qualitative.Dark24
+    colors = qualitative.Dark24  # todo these heights are still messed up
     for ind in range(12):
         row = ind // 3 + 1
         col = ind % 3 + 1
@@ -638,3 +640,48 @@ def analyze_samples(x, mol_list, max_z_prime, device, batch_size):
                 pbar.update(batch_size)
 
     return samples
+
+
+def cluster_hdbscan_to_df(X: torch.Tensor,
+                          lj_pot: torch.Tensor,
+                          min_cluster_size: int = 10):
+    """
+    Run HDBSCAN on samples X [n, d] and summarize results in a DataFrame.
+
+    Parameters
+    ----------
+    X : torch.Tensor [n, d]
+        Sample coordinates.
+    lj_pot : torch.Tensor [n]
+        Per-sample energies.
+    min_cluster_size : int
+        Minimum cluster size for HDBSCAN.
+
+    Returns
+    -------
+    pd.DataFrame with columns ['cluster_id', 'n', 'p', 'mean_en'].
+    """
+
+    # --- run clustering on CPU numpy array ---
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size)
+    labels = clusterer.fit_predict(X.cpu().numpy())
+
+    # cluster probabilities (membership strengths)
+
+    # --- collect cluster masks and statistics ---
+    df_records = []
+    unique_labels = np.unique(labels)
+    for cid in unique_labels:
+        mask = labels == cid
+        n_points = int(mask.sum())
+        p_mean = n_points / len(X)
+
+        if cid == -1:  # noise cluster
+            mean_en = float(torch.tensor(log_rescale_positive(lj_pot[mask])).mean().cpu())
+        else:
+            mean_en = float(log_rescale_positive(lj_pot[mask]).mean().cpu())
+
+        df_records.append(dict(cluster_id=cid, n=n_points, p=p_mean, mean_en=mean_en))
+
+    top_df = pd.DataFrame(df_records, columns=['cluster_id', 'n', 'p', 'mean_en'])
+    return top_df, labels

energy_sampling/train.py
@@ -61,6 +61,8 @@ class Modeller:
         self.grow_buffer = self.args.grow_buffer
         if self.args.anchor_fwd_bwd:
             self.args.fwd_to_bwd_ratio = 1.0E-6
+        self.forward_batch_size = self.args.batch_size
+        self.backward_batch_size = self.args.batch_size
 
     def train_logic(self, buffer, it):
         do_forward = False
@@ -91,10 +93,6 @@ class Modeller:
             do_forward = True
             do_backward = False
 
-        if it % 21 == 0:
-            report_losses = True
-        else:
-            report_losses = False
 
         if not any([
             self.args.bwd_loss_coeffs.tb > 0,
@@ -106,7 +104,13 @@ class Modeller:
             do_backward = False
             do_forward = True
 
-        return do_backward, do_forward, p_forward, report_losses
+        if do_forward:
+            step_type = "Forward"
+        elif do_backward:
+            step_type = "Backward"
+        else:
+            assert False
+        return step_type
 
     def log_elapsed_times(self):
         elapsed_times = {}
@@ -144,30 +148,38 @@ class Modeller:
             assert False
         return discretizer
 
-    def increment_batch_size(self, buffer, train_mol_loader, test_mol_loader, batch_growth_increment):
-        new_batch_size = max(self.args.batch_size + 1,
-                             int(self.args.batch_size * batch_growth_increment))
-        self.args.batch_size = new_batch_size  # gradually increment batch size
+    def increment_batch_size(self, buffer, train_mol_loader, test_mol_loader, batch_growth_increment, step_type,
+                             train_iterator, test_iterator):
+        if step_type == "Forward":
+            if self.forward_batch_size < self.args.max_batch_size:
+                new_batch_size = min(self.args.max_batch_size, max(self.forward_batch_size + 1,
+                                                                   int(self.forward_batch_size * batch_growth_increment)))
+                self.forward_batch_size = new_batch_size  # gradually increment batch size
 
-        if len(buffer) > 0:
-            buffer.batch_size = new_batch_size
+                if len(buffer) > 0:
+                    buffer.batch_size = new_batch_size
 
-        train_mol_loader = DataLoader(
-            train_mol_loader.dataset,
-            batch_size=new_batch_size,
-            num_workers=0,
-            pin_memory=True,
-            drop_last=True,
-        )
-        test_mol_loader = DataLoader(
-            test_mol_loader.dataset,
-            batch_size=new_batch_size,
-            num_workers=0,
-            pin_memory=True,
-            drop_last=True,
-        )
-        train_iterator = iter_forever(train_mol_loader)
-        test_iterator = iter_forever(test_mol_loader)
+                train_mol_loader = DataLoader(
+                    train_mol_loader.dataset,
+                    batch_size=new_batch_size,
+                    num_workers=0,
+                    pin_memory=True,
+                    drop_last=True,
+                )
+                test_mol_loader = DataLoader(
+                    test_mol_loader.dataset,
+                    batch_size=new_batch_size,
+                    num_workers=0,
+                    pin_memory=True,
+                    drop_last=True,
+                )
+                train_iterator = iter_forever(train_mol_loader)
+                test_iterator = iter_forever(test_mol_loader)
+        elif step_type == "Backward":
+            if self.backward_batch_size < self.args.max_batch_size:
+                new_batch_size = min(self.args.max_batch_size, max(self.backward_batch_size + 1,
+                                                                   int(self.backward_batch_size * batch_growth_increment)))
+                self.backward_batch_size = new_batch_size  # gradually increment batch size
 
         return buffer, train_mol_loader, test_mol_loader, train_iterator, test_iterator
 
@@ -250,9 +262,7 @@ class Modeller:
             'temperature_conditioning': self.args.temperature_conditioning,
             'temperature': self.args.energy_static_temperature,
             'density_coeff': self.args.energy_density_coeff,
-            'energy_clip': self.args.energy_clip,
             'lj_coeff': self.args.energy_lj_coeff,
-            'lj_turnover_pot': self.args.lj_turnover_pot,
             'molecule_conditioning': self.args.molecule_conditioning,
             'sg_conditioning': self.args.sg_conditioning,
             'space_groups': self.args.space_groups,
@@ -261,6 +271,8 @@ class Modeller:
             'z_primes': self.args.z_primes,
             'max_z_prime': max(self.args.z_primes),
             'zp_conditioning': self.args.zp_conditioning,
+            'uma_path': self.args.uma_path,
+            'reward_range': self.args.reward_range,
         }
         energy_function = MolecularCrystal(**energy_config)
         return energy_function
@@ -269,7 +281,8 @@ class Modeller:
         if self.args.checkpoint_path is not None:
             print(f"Loading model from checkpoint {self.args.checkpoint_path}")
             eval_path = self.args.checkpoint_path.replace('train', 'eval')
-            config_path = self.args.checkpoint_path.replace('train', 'config').replace('.pt', '.npy').replace('_hit_prior','').replace('_thermalized','')
+            config_path = self.args.checkpoint_path.replace('train', 'config').replace('.pt', '.npy').replace(
+                '_hit_prior', '').replace('_thermalized', '')
 
             gfn_config = np.load(config_path, allow_pickle=True).item()
             gfn_model = GFN(**gfn_config).to(self.device)
@@ -368,7 +381,7 @@ class Modeller:
             self.args.buffer_size,
             'cpu',
             energy_function,
-            self.args.batch_size,
+            self.backward_batch_size,
             beta=self.args.beta,
             rank_weight=self.args.rank_weight,
             prioritized=self.args.prioritized,
@@ -429,14 +442,14 @@ class Modeller:
 
         train_mol_loader = DataLoader(
             train_mols_list,
-            batch_size=self.args.batch_size,
+            batch_size=self.forward_batch_size,
             num_workers=0,
             pin_memory=True,
             drop_last=True
         )
         test_mol_loader = DataLoader(
             test_mols_list,
-            batch_size=self.args.batch_size,
+            batch_size=self.forward_batch_size,
             num_workers=0,
             pin_memory=True,
             drop_last=True
@@ -594,15 +607,18 @@ class Modeller:
 
                 self.times['train_step_start'] = time()
                 try:
-                    train_loss, step_type, loss_dict = self.train_step(energy_function,
-                                                                       gfn_model,
-                                                                       optimizers,
-                                                                       step_ind,
-                                                                       exploration_std,
-                                                                       buffer,
-                                                                       train_iterator,
-                                                                       repeats=self.args.repeats
-                                                                       )
+                    step_type = self.train_logic(buffer, step_ind)
+                    train_loss, loss_dict = self.train_step(
+                        step_type,
+                        energy_function,
+                        gfn_model,
+                        optimizers,
+                        step_ind,
+                        exploration_std,
+                        buffer,
+                        train_iterator,
+                        repeats=self.args.repeats
+                    )
                     if self.args.ema_decay is not None:
                         update_ema(gfn_model, ema_model, decay=self.args.ema_decay)
                     else:
@@ -617,12 +633,14 @@ class Modeller:
                         if loss_dict is not None:
                             bwd_loss_dict = loss_dict
 
-                    if not oomed_out and (
-                            self.args.batch_size < self.args.max_batch_size and self.args.grow_batch_size):
+                    if not oomed_out and self.args.grow_batch_size:
                         buffer, train_mol_loader, test_mol_loader, train_iterator, test_iterator = self.increment_batch_size(
                             buffer, train_mol_loader,
                             test_mol_loader,
-                            self.args.batch_growth_increment)
+                            self.args.batch_growth_increment,
+                            step_type,
+                            train_iterator,
+                            test_iterator)
 
                 except (RuntimeError, ValueError) as e:  # if we do hit OOM, slash the batch size
                     (oomed_out, buffer, train_mol_loader,
@@ -630,14 +648,36 @@ class Modeller:
                         e, oomed_out, buffer,
                         train_mol_loader,
                         test_mol_loader,
-                        optimizers)
+                        optimizers, step_type
+                    )
                 self.times['train_step_end'] = time()
 
                 # evaluation work
                 if (step_ind % self.args.eval_period == 0 and step_ind > 0) or step_ind == 50:
-                    self.eval_work(ema_model, step_ind,
-                                   buffer, train_mol_loader, test_mol_loader,
-                                   energy_function, metrics)
+                    finished = False
+                    while not finished:
+                        try:
+                            self.eval_work(ema_model, step_ind,
+                                           buffer, train_mol_loader, test_mol_loader,
+                                           energy_function, metrics)
+                            finished = True
+                        except (RuntimeError, ValueError) as e:
+                            print(f"Caught error: {str(e)}")
+                            if is_cuda_oom(e):
+                                self.args.eval_batch_size = max(1, int(self.args.eval_batch_size * 0.95))
+                                if self.args.eval_batch_size <= 1:
+                                    raise RuntimeError("Cascading OOM Failure")
+                                print(f"Reducing eval batch size to {self.args.eval_batch_size}")
+                                gc.collect()
+                                if torch.cuda.is_available():
+                                    torch.cuda.empty_cache()
+                                    try:
+                                        torch.cuda.ipc_collect()
+                                    except Exception:
+                                        pass
+                            else:
+                                raise e
+
                     if self.args.anchor_fwd_bwd and self.args.both_ways:
                         self.manage_prior_anchor(step_ind, metrics, gfn_model, ema_model, name)
 
@@ -730,6 +770,7 @@ class Modeller:
                     p.register_hook(lambda g, n=p_name: grad_check_hook(g, n))
 
     def train_step(self,
+                   step_type,
                    energy_function,
                    gfn_model,
                    optimizers,
@@ -738,7 +779,19 @@ class Modeller:
                    buffer,
                    mol_iterator,
                    repeats):
-        do_backward, do_forward, p_forward, report_losses = self.train_logic(buffer, it)
+        if step_type == "Forward":
+            do_forward=True
+            do_backward=False
+        elif step_type=="Backward":
+            do_forward=False
+            do_backward=True
+        else:
+            assert False
+
+        if it % 21 == 0:
+            report_losses = True
+        else:
+            report_losses = False
 
         discretizer = self.get_discretizer()
 
@@ -789,8 +842,6 @@ class Modeller:
             optimizers['bwd'].step()
             optimizers['flow'].step()
 
-        step_type = "Forward" if do_forward else "Backward"
-
         if report_losses:
             loss_dict_cpu = {step_type + "_loss/" + key: value.cpu().detach().numpy() for key, value in
                              loss_dict.items()}
@@ -799,13 +850,13 @@ class Modeller:
 
         del loss, loss_dict  # or whatever is large
 
-        return clean_loss, step_type, loss_dict_cpu
+        return clean_loss, loss_dict_cpu
 
     def fwd_train_step(self, energy_function, gfn_model, discretizer,
                        exploration_std, mol_batch, buffer, return_exp=False,
                        repeats: int = 10,
                        report_losses: bool = False):
-        init_state = get_gfn_init_state(self.args.batch_size, energy_function.data_ndim, self.device)
+        init_state = get_gfn_init_state(self.forward_batch_size, energy_function.data_ndim, self.device)
         log_T_tensor, sg_inds, condition = energy_function.get_conditioning_tensor(mol_batch,
                                                                                    z_primes=mol_batch.z_prime)
         mol_batch.sg_ind = sg_inds
@@ -828,7 +879,7 @@ class Modeller:
                        report_losses: bool = False):
         if self.args.sampling == 'buffer':
             samples, rewards, crystal_batch, condition = buffer.sample(
-                override_batch=int(self.args.batch_size * self.args.bwd_batch_multiplier),
+                override_batch=int(self.backward_batch_size * self.args.bwd_batch_multiplier),
                 randomize_orientations=True if self.args.molecule_conditioning else False,
             )
         else:
@@ -848,7 +899,7 @@ class Modeller:
                                      repeats=repeats,
                                      report_losses=report_losses)
 
-    def handle_train_epoch_error(self, e, oomed_out, buffer, train_mol_loader, test_mol_loader, optimizers):
+    def handle_train_epoch_error(self, e, oomed_out, buffer, train_mol_loader, test_mol_loader, optimizers, step_type):
         print(f"Caught error: {str(e)}")
         if is_cuda_oom(e):
             print("OOMED!")
@@ -865,32 +916,38 @@ class Modeller:
                 except Exception:
                     pass
 
-            self.args.batch_size = max(1, int(self.args.batch_size * 0.95))
-            if self.args.batch_size <= 1:
-                raise RuntimeError("Cascading OOM Failure")
+            if step_type == 'Forward':
+                self.forward_batch_size = max(1, int(self.forward_batch_size * 0.95))
+                if self.forward_batch_size <= 1:
+                    raise RuntimeError("Cascading OOM Failure")
+                train_mol_loader = DataLoader(
+                    train_mol_loader.dataset,
+                    batch_size=self.forward_batch_size,
+                    num_workers=0,
+                    pin_memory=True,
+                    drop_last=True,
+                )
+                test_mol_loader = DataLoader(
+                    test_mol_loader.dataset,
+                    batch_size=self.forward_batch_size,
+                    num_workers=0,
+                    pin_memory=True,
+                    drop_last=True,
+                )
+                train_iterator = iter_forever(train_mol_loader)
+                test_iterator = iter_forever(test_mol_loader)
+                print(f"Reducing forward batch size to {self.forward_batch_size}")
+
+            elif step_type == 'Backward':
+                self.backward_batch_size = max(1, int(self.backward_batch_size * 0.95))
+                if self.backward_batch_size <= 1:
+                    raise RuntimeError("Cascading OOM Failure")
+                print(f"Reducing backward batch size to {self.backward_batch_size}")
 
             gc.collect()
             torch.cuda.empty_cache()
 
-            train_mol_loader = DataLoader(
-                train_mol_loader.dataset,
-                batch_size=self.args.batch_size,
-                num_workers=0,
-                pin_memory=True,
-                drop_last=True,
-            )
-            test_mol_loader = DataLoader(
-                test_mol_loader.dataset,
-                batch_size=self.args.batch_size,
-                num_workers=0,
-                pin_memory=True,
-                drop_last=True,
-            )
-            train_iterator = iter_forever(train_mol_loader)
-            test_iterator = iter_forever(test_mol_loader)
-
             oomed_out = True
-            print(f"Reducing batch size to {self.args.batch_size}")
         else:
             raise e  # will simply raise error if other or if training on CPU
         return oomed_out, buffer, train_mol_loader, test_mol_loader, train_iterator, test_iterator
@@ -932,7 +989,8 @@ class Modeller:
                       save_batch=self.grow_buffer,
                       ))
 
-        eval_metrics.update({'Batch Size': self.args.batch_size})
+        eval_metrics.update({'Forward Batch Size': self.forward_batch_size})
+        eval_metrics.update({'Backward Batch Size': self.backward_batch_size})
         eval_metrics.update(self.log_elapsed_times())
 
         self.times['eval_step_end'] = time()
@@ -1013,12 +1071,30 @@ class Modeller:
         good_inds = torch.argwhere(torch.all(latents.abs() <= 1, dim=1))  # valid latent space
         dataset = [dataset[ind] for ind in good_inds]
 
-        if self.args.energy_function in ['silu', 'lj', 'qlj']:  # reparameterize incoming samples
+        # filter near-identical samples
+        d_cut = 0.01
+        latents = collate_data_list(dataset).latent_params()
+        dmat = torch.cdist(latents, latents)
+        keep = torch.zeros(len(latents), dtype=bool, device=latents.device)
+
+        for i in range(len(latents)):
+            # check if this point is far from all previously kept points
+            if not (dmat[i, keep] < d_cut).any():
+                keep[i] = True
+
+        keep_inds = torch.arange(len(latents), device=latents.device)[keep]
+        dataset = [dataset[ind] for ind in keep_inds]
+
+        # todo remove!!
+        dataset = dataset[:100]
+
+        if self.args.energy_function in ['silu', 'lj', 'qlj', 'uma']:  # reparameterize incoming samples
             print("Re-featurizing preloaded buffer samples")
             dataset = featurize_dataset(dataset,
                                         self.device,
                                         self.args.energy_function,
-                                        max_z_prime=max_z_prime)
+                                        max_z_prime=max_z_prime,
+                                        uma_path=self.args.uma_path)
 
         if filter_unbound:  # filter unbound states
             dataset = [elem for elem in dataset if elem.lj_pot < 0]

energy_sampling/utils.py
@@ -6,7 +6,9 @@ import queue
 import random
 import threading
 from argparse import Namespace
+from asyncio import sleep
 from pathlib import Path
+from typing import Optional
 
 import PIL
 import numpy as np
@@ -23,6 +25,7 @@ from mxtaltools.common.utils import log_rescale_positive
 from mxtaltools.crystal_building.crystal_latent_transforms import enforce_niggli_plane
 from mxtaltools.dataset_utils.data_classes import MolCrystalData
 from mxtaltools.dataset_utils.utils import collate_data_list
+from mxtaltools.mlip_interfaces.uma_utils import init_uma_crystal_predictor
 from mxtaltools.models.utils import load_encoder
 
 
@@ -258,36 +261,69 @@ def triangle_schedule(it, init, maxval, minval, on, off):
 
 @torch.no_grad()
 def featurize_dataset(dataset, device, energy_function: str, batch_size: int = 500,
-                      max_z_prime: int = 1):
-    num_batches = len(dataset) // batch_size + (1 if len(dataset) % batch_size > 0 else 0)
+                      max_z_prime: int = 1, uma_path: Optional[str] = None,):
+
     silus = []
     ljs = []
     qljs = []
     niggli_overlaps = []
-    if energy_function in ['lj', 'qlj']:
-        cutoff = 10
-    elif energy_function == 'silu':
-        cutoff = 6
-    else:
-        assert False
+    gas_umas = []
+    cry_umas = []
+    cutoff = 10
 
-    for b_ind in range(num_batches):
-        crystal_batch = collate_data_list(dataset[b_ind * batch_size:(b_ind + 1) * batch_size], max_z_prime=max_z_prime)
-        crystal_batch = crystal_batch.to(device)
-        crystal_batch.box_analysis()
-        out = crystal_batch.analyze(['lj', 'qlj', 'silu', 'niggli'],
-                                    cutoff=cutoff,
-                                    supercell_size=5,
-                                    std_orientation=True)
+    if energy_function == 'uma':
+        uma_predictor = init_uma_crystal_predictor(uma_path, device=device)
 
-        silus.extend(out['silu'].cpu().detach())
-        ljs.extend(out['lj'].cpu().detach())
-        qljs.extend(out['qlj'].cpu().detach())
-        niggli_overlaps.extend(out['niggli'].cpu().detach())
+
+    cursor = 0
+    pbar = tqdm(total=len(dataset), unit="reparameterized samples")
+
+    while cursor < len(dataset):
+        try:
+            crystal_batch = collate_data_list(
+                [dataset[ind] for ind in range(cursor, min(len(dataset), cursor + batch_size))])
+            crystal_batch = crystal_batch#.to(device)
+
+            if energy_function == 'uma':
+                cry_umas.extend(crystal_batch.compute_crystal_uma(
+                    predictor=uma_predictor, std_orientation=True).cpu().detach() * 96.485)  # output in kJ/mol (of unit cells)
+                gas_umas.extend(crystal_batch.compute_lattice_gas_phase_uma(
+                    predictor=uma_predictor, std_orientation=True).cpu().detach() * 96.485)
+            else:
+                cry_umas.extend(torch.ones(crystal_batch.num_graphs, dtype=torch.float32, device='cpu'))
+                gas_umas.extend(torch.ones(crystal_batch.num_graphs, dtype=torch.float32, device='cpu'))
+
+            crystal_batch.box_analysis()
+            out = crystal_batch.analyze(['lj', 'qlj', 'silu', 'niggli'],
+                                        cutoff=cutoff,
+                                        supercell_size=5,
+                                        std_orientation=True)
+
+            silus.extend(out['silu'].cpu().detach())
+            ljs.extend(out['lj'].cpu().detach())
+            qljs.extend(out['qlj'].cpu().detach())
+            niggli_overlaps.extend(out['niggli'].cpu().detach())
+
+            cursor += batch_size
+            pbar.update(min(batch_size, len(dataset) - cursor))  # safe final update
+            batch_size += 1
+
+        except (RuntimeError, ValueError) as e:
+            if is_cuda_oom(e):
+                batch_size = max(int(batch_size * 0.6), 1)
+                print(f"OOM error: dropping batch size to {batch_size}")
+                gc.collect()
+                torch.cuda.empty_cache()
+                torch.cuda.synchronize()
+                sleep(0.1)
+            else:
+                raise e
 
     silus = torch.tensor(silus)
     ljs = torch.tensor(ljs)
     qljs = torch.tensor(qljs)
+    gas_umas = torch.tensor(gas_umas)
+    cry_umas = torch.tensor(cry_umas)
     niggli_overlaps = torch.tensor(niggli_overlaps)
     for ind, elem in enumerate(dataset):
         elem.silu_pot = torch.ones(1) * silus[ind]
@@ -295,6 +331,8 @@ def featurize_dataset(dataset, device, energy_function: str, batch_size: int = 5
         elem.qlj_pot = torch.ones(1) * qljs[ind]
         elem.niggli_overlap = torch.ones(1) * niggli_overlaps[ind]
         elem.scaled_lj_pot = torch.ones(1) * log_rescale_positive(ljs[ind])
+        elem.uma_gas_pot = torch.ones(1) * gas_umas[ind]
+        elem.uma_pot = torch.ones(1) * cry_umas[ind]
 
     # exclude negative niggli overlaps
     dataset = [elem for elem in dataset if elem.niggli_overlap >= 0]
@@ -325,6 +363,8 @@ def embed_dataset(dataset, autoencoder_path=None, device=None, encoder=None, emb
                                       target_handedness=torch.ones_like(crystal_batch.radius)[:, None],
                                       )
         if embedding_type == 'autoencoder':
+            # NOTE - autoencoder trained on global centroids, but recenter molecules in orient_molecule works on heavy atoms now
+            crystal_batch.recenter_molecules(center_on_heavy_atoms=False)
             embeddings.append(encoder.encode(crystal_batch).clone().cpu())
         elif embedding_type == 'principal_axes':
             v_embedding_i, s_embedding_i, _ = batch_molecule_principal_axes_torch(
@@ -332,6 +372,8 @@ def embed_dataset(dataset, autoencoder_path=None, device=None, encoder=None, emb
                 crystal_batch.batch,
                 crystal_batch.num_graphs,
                 crystal_batch.num_atoms,
+                heavy_atoms_only=True,
+                atom_types=crystal_batch.z
             )
             embeddings.append((v_embedding_i * s_embedding_i[:, :, None]).cpu())
 
@@ -532,64 +574,6 @@ def iter_forever(loader):
         for batch in loader:
             yield batch
 
-
-class ThreadedDataLoader:
-    def __init__(self, dataset, batch_size=1, max_prefetch=2, collate_fn=None, pin_memory=True):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.max_prefetch = max_prefetch
-        self.collate_fn = collate_fn or (lambda x: x)
-        self.queue = queue.Queue(max_prefetch)
-        # self.queue = queue.SimpleQueue()
-
-        self.stop_event = threading.Event()
-        self.pin_memory = pin_memory
-        self.skip_next = False
-
-        self.thread = threading.Thread(target=self._worker, daemon=True)
-        self.thread.start()
-
-    def _worker(self):
-        n = len(self.dataset)
-        while not self.stop_event.is_set():
-            # sample with replacement
-            idxs = torch.randint(n, (self.batch_size,), device='cpu')
-            batch = [self.dataset[i] for i in idxs]
-            batch = self.collate_fn(batch)
-            if self.pin_memory:
-                batch = batch.pin_memory()
-            self.queue.put(batch)
-
-    def __iter__(self):
-        return self
-
-    def shutdown(self):
-        self.stop_event.set()
-        if self.thread.is_alive():
-            self.thread.join()
-
-    def __next__(self):
-        while True:
-            batch = self.queue.get()
-            if self.skip_next:
-                # discard this one batch no matter what
-                self.skip_next = False
-                continue
-            return batch
-
-    def set_batch_size(self, new_size):
-        """Update batch size and flush queue to avoid stale batches."""
-        self.batch_size = int(new_size)
-        self._flush_queue()
-        self.skip_next = True  # force burn one
-
-    def _flush_queue(self):
-        """Remove all pending items in the prefetch queue."""
-        while not self.queue.empty():
-            try:
-                self.queue.get_nowait()
-            except queue.Empty:
-                break
 
 
 def is_cuda_oom(e: Exception) -> bool: