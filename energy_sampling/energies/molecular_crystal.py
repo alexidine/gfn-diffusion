:> z'>1 edge construction fix :> explicit jacobian in rotational latent space :> new configs :> speedups

energy_sampling/energies/molecular_crystal.py
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
+from mxtaltools.common.geometry_utils import lat2sph_rotvec
 from mxtaltools.common.utils import log_rescale_positive, is_cuda_oom
 from mxtaltools.constants.space_group_feature_tensor import SG_FEATURE_TENSOR
 from mxtaltools.constants.space_group_info import SYM_OPS
@@ -110,7 +111,8 @@ class MolecularCrystal(BaseSet):
         max_reward = max(dataset_rewards)
         reward_range = self.reward_range
         min_allowed_reward = max_reward - reward_range
-        self.energy_clip = float(- min_allowed_reward * self.temperature)  # convert the minimum allowed reward to a clip on the energy
+        self.energy_clip = float(
+            - min_allowed_reward * self.temperature)  # convert the minimum allowed reward to a clip on the energy
         self.reward_clip = min_allowed_reward
 
     def instantiate_crystals(self, x, mol_batch):
@@ -118,7 +120,7 @@ class MolecularCrystal(BaseSet):
         crystal_batch.latent_to_cell_params(x)
         return crystal_batch
 
-    def analyze_crystal_batch(self, x, mol_batch, return_batch=False):  # x is gfn_outputs
+    def analyze_crystal_batch(self, x, mol_batch,temperature, return_batch=False):  # x is gfn_outputs
         crystal_batch = self.instantiate_crystals(x, mol_batch)
 
         cutoff = 10
@@ -137,15 +139,15 @@ class MolecularCrystal(BaseSet):
                 uma_energy = crystal_batch.compute_crystal_uma(
                     predictor=self.uma_predictor,
                     std_orientation=False) * 96.485  # output in kJ/mol (of unit cells)
-                out.update({'uma_pot': uma_energy,
-                            'uma': uma_energy / (
-                                    crystal_batch.sym_mult * crystal_batch.z_prime) - crystal_batch.uma_gas_pot})
+                out.update({'uma_pot': uma_energy.float(),
+                            'uma': (uma_energy / (
+                                    crystal_batch.sym_mult * crystal_batch.z_prime) - crystal_batch.uma_gas_pot).float()})
 
         for key in out.keys():
             crystal_batch.add_graph_attr(out[key], key)
         crystal_batch.add_graph_attr(log_rescale_positive(out['lj']), 'scaled_lj')
 
-        crystal_energy, ens_dict = self.generator_energy(crystal_batch, raw_latents=x)
+        crystal_energy, ens_dict = self.generator_energy(crystal_batch, temperature, raw_latents=x)
 
         crystal_batch.add_graph_attr(crystal_energy, 'gfn_energy')
 
@@ -161,7 +163,7 @@ class MolecularCrystal(BaseSet):
         else:
             return crystal_energy
 
-    def generator_energy(self, crystal_batch, raw_latents=None):
+    def generator_energy(self, crystal_batch, temperature, raw_latents=None):
         ens_dict = {}
 
         latents = crystal_batch.latent_params()
@@ -222,16 +224,32 @@ class MolecularCrystal(BaseSet):
 
         else:
             assert False, f'{self.energy_function} not implemented'
+
+        latent_rotvecs = crystal_batch.latent_params()[:, -3*crystal_batch.max_z_prime:]
+        sph_rotvec = lat2sph_rotvec(latent_rotvecs, crystal_batch.max_z_prime)
+        sph = sph_rotvec.view(crystal_batch.num_graphs, crystal_batch.max_z_prime, 3)
+        phi = sph[..., 0]  # polar angle
+        r = sph[..., 2]  # rotation magnitude
+        eps = 1e-8
+        jacobian_weight = (
+                torch.sin(r / 2).clamp_min(eps) ** 2
+                * torch.sin(phi).clamp_min(eps)
+        )
+        jacobian_energy = - temperature * torch.log(jacobian_weight.sum(dim=-1))  # sum, because each dim gets its own correction
+
         if self.energy_clip is not None:
 
-            total_energy = log_rescale_positive(crystal_energy,
-                                                self.energy_clip) + bounding_energy * self.bounding_coeff + reduction_energy * self.reduction_coeff
-            return (log_rescale_positive(total_energy, self.energy_clip + 0.1 * np.abs(self.energy_clip)),
+            total_energy = (log_rescale_positive(crystal_energy, self.energy_clip) +
+                            bounding_energy * self.bounding_coeff +
+                            reduction_energy * self.reduction_coeff)
+            return (log_rescale_positive(total_energy, self.energy_clip + 0.1 * np.abs(self.energy_clip)) + jacobian_energy,
                     # to prevent total saturation by the energy function, add a buffer over the clip
                     ens_dict)
         else:
-
-            total_energy = crystal_energy + bounding_energy * self.bounding_coeff + reduction_energy * self.reduction_coeff
+            total_energy = (crystal_energy +
+                            bounding_energy * self.bounding_coeff +
+                            reduction_energy * self.reduction_coeff +
+                            jacobian_energy)
             return total_energy, ens_dict
 
     def compute_zp_order_penalty(self, bounding_energy, crystal_batch):
@@ -316,7 +334,7 @@ class MolecularCrystal(BaseSet):
         else:
             crystal_batch = crystals
 
-        energy, _ = self.generator_energy(crystal_batch)
+        energy, _ = self.generator_energy(crystal_batch, temperature)
 
         if torch.is_tensor(temperature):
             sample_temperature = temperature.to(crystal_batch.device)
@@ -341,17 +359,16 @@ class MolecularCrystal(BaseSet):
         :param x:
         :return:
         """
-
-        energy, crystal_batch = self.batched_analyze_crystal_batch(x, mol_batch, return_batch=True)
         temperature = 10 ** log_temperature
-        sample_temperature = temperature
+        energy, crystal_batch = self.batched_analyze_crystal_batch(x, mol_batch, temperature, return_batch=True)
+
 
         if return_exp:
-            return energy / sample_temperature, crystal_batch
+            return energy / temperature, crystal_batch
         else:
-            return energy / sample_temperature
+            return energy / temperature
 
-    def batched_analyze_crystal_batch(self, x, mol_batch, return_batch=False):
+    def batched_analyze_crystal_batch(self, x, mol_batch, temperature, return_batch=False):
         if not hasattr(self, 'batch_size'):
             self.batch_size = 1000 if self.energy_function != 'uma' else 50
         cursor = 0
@@ -368,19 +385,20 @@ class MolecularCrystal(BaseSet):
                 inds = np.arange(cursor, min(n_samples, cursor + self.batch_size))
                 mol_batch_i = collate_data_list([data_list[ind] for ind in inds])
 
-                outs = self.analyze_crystal_batch(x[inds], mol_batch_i, return_batch=return_batch)
+                outs = self.analyze_crystal_batch(x[inds], mol_batch_i, temperature[inds], return_batch=return_batch)
 
                 energies[inds] = outs[0].cpu().detach()
                 if return_batch:
                     samples.extend(outs[1].cpu().detach().batch_to_list())
 
                 cursor += len(inds)
-                self.batch_size += max(int(self.batch_size * 0.01), 1)
+                if self.batch_size <= 10000:
+                    self.batch_size += max(int(self.batch_size * 0.01), 1)
 
             except (RuntimeError, ValueError) as e:
                 if is_cuda_oom(e):
                     self.batch_size = max(int(self.batch_size * 0.8), 1)
-                    print(f"OOM in energy evaluation: dropping batch size to {self.batch_size}")
+                    #print(f"OOM in energy evaluation: dropping batch size to {self.batch_size}")
                     gc.collect()
                     torch.cuda.empty_cache()
                     torch.cuda.synchronize()

energy_sampling/eval/evaluations.py
@@ -644,9 +644,10 @@ def cluster_fig(sample_embedding, anchor_embedding, cluster_ind, anchor_energies
 
 
 @torch.no_grad()
-def bwd_evaluation(buffer, gfn_model, init_state, discretizer, do_figs: Optional[bool] = False):
+def bwd_evaluation(buffer, gfn_model, discretizer, batch_size, do_figs: Optional[bool] = False):
+
     log_z, b_log_pbs, b_log_pfs, b_means_b, b_means_f, b_vars_b, b_vars_f, backward_flow_states, log_r = analyze_buffer(
-        buffer, discretizer, gfn_model, init_state)
+        buffer, discretizer, gfn_model, batch_size)
 
     metrics = {}
     fig_dict = {}
@@ -691,8 +692,11 @@ def bwd_evaluation(buffer, gfn_model, init_state, discretizer, do_figs: Optional
     metrics['Bwd Normed Log Z LB Residual'] = ((log_Z_lb - log_Z_learned).abs() / log_Z_lb.abs()).item()
 
     if do_figs:
-        fig_dict = bwd_figs(b_log_pbs, b_log_pfs, b_means_b, b_means_f, b_vars_b, b_vars_f, backward_flow_states, fig_dict, log_r,
-                 log_z)
+        fig_dict = bwd_figs(b_log_pbs, b_log_pfs,
+                            b_means_b, b_means_f, b_vars_b, b_vars_f,
+                            backward_flow_states,
+                            fig_dict,
+                            log_r, log_z)
 
     return metrics, fig_dict, log_r, tb_residual, normed_tb_residual
 
@@ -719,10 +723,9 @@ def bwd_figs(b_log_pbs, b_log_pfs, b_means_b, b_means_f, b_vars_b, b_vars_f, bac
     return fig_dict
 
 
-def analyze_buffer(buffer, discretizer, gfn_model, init_state):
+def analyze_buffer(buffer, discretizer, gfn_model, batch_size):
     sample_inds = torch.arange(len(buffer))
     num_samples = len(sample_inds)
-    batch_size = len(init_state)
     num_batches = num_samples // batch_size + int((num_samples % batch_size) != 0)
     acc = dict(
         backward_flow_states=[],
@@ -744,7 +747,7 @@ def analyze_buffer(buffer, discretizer, gfn_model, init_state):
         inds = sample_inds[start:end]
 
         terminal_state, log_r, crystal_batch, condition = buffer.sample(
-            override_batch=len(init_state),
+            override_batch=batch_size,
             override_sample_inds=inds
         )
 
@@ -913,7 +916,7 @@ def log_metrics(energy_function,
                 for p in np.linspace(0, 1, 50)
             ])
             metrics['Buffer Noised Length'] = buffer.noised_size
-            metrics['Buffer Noised Mean Reward'] = torch.mean(buffer.noised_rewards)
+            metrics['Buffer Noised Mean Reward'] = torch.mean(buffer.noised_rewards[:buffer.noised_size])
             metrics['Buffer Mean Score'] = np.mean(np.nan_to_num(buffer.rewards_list))
 
             (buffer_cell_params, buffer_latent_params,

energy_sampling/train.py
@@ -157,7 +157,10 @@ class Modeller:
             assert False
         return discretizer
 
-    def increment_batch_size(self, buffer, train_mol_loader, test_mol_loader, batch_growth_increment, step_type,
+    def increment_batch_size(self, buffer,
+                             train_mol_loader, test_mol_loader,
+                             batch_growth_increment,
+                             step_type,
                              train_iterator, test_iterator):
         if step_type == "Forward":
             # print("7")
@@ -662,7 +665,7 @@ class Modeller:
                         if loss_dict is not None:
                             bwd_loss_dict = loss_dict
 
-                    if not oomed_out and self.args.grow_batch_size:
+                    if (not oomed_out or (step_ind % 100 == 0)) and self.args.grow_batch_size:
                         (buffer, train_mol_loader, test_mol_loader,
                          train_iterator, test_iterator) = self.increment_batch_size(
                             buffer, train_mol_loader,
@@ -983,7 +986,7 @@ class Modeller:
                     reward_range = 5
                     reward_record, sample_record = noise_buffer(0.5, 1,
                                                                 buffer, energy_function, reward_range,
-                                                                noise_step = 0.025,
+                                                                noise_step=0.025,
                                                                 sample_inds=None)
 
                     reward_record = torch.tensor(reward_record)
@@ -1105,7 +1108,6 @@ class Modeller:
         print("Loading prebuilt buffer")
         dataset = torch.load(dataset_path, weights_only=False)
 
-        # todo exclude outside latents and warn if there are a shitload of them
         max_z_prime = max([int(elem.z_prime) for elem in dataset])
         assert max_z_prime == max(self.args.z_primes), "Preloaded data max z prime must match model"
 
@@ -1149,6 +1151,9 @@ class Modeller:
         keep_inds = torch.arange(len(latents), device=latents.device)[keep]
         dataset = [dataset[ind] for ind in keep_inds]
 
+        dataset = [elem for elem in dataset if elem.packing_coeff >= 0.55]
+        dataset = [elem for elem in dataset if elem.packing_coeff <= 0.95]
+
         # todo remove this eventually
         if 'D:' in self.args.buffer_path and self.args.energy_function == 'uma':  # if we're on local, this takes forever
             dataset = dataset[:500]
@@ -1161,8 +1166,6 @@ class Modeller:
 
         # always filter awful crystals
         dataset = [elem for elem in dataset if elem.reduction_en <= 1e-3]
-        dataset = [elem for elem in dataset if elem.packing_coeff >= 0.55]
-        dataset = [elem for elem in dataset if elem.packing_coeff <= 0.95]
 
         if filter_unbound:  # filter unbound states under this potential
             en_func = self.args.energy_function
@@ -1228,45 +1231,18 @@ class Modeller:
 
         '''bwd sampling and analysis are combined'''
         if self.args.both_ways or self.args.bwd:
-            init_state = get_gfn_init_state(self.args.eval_num_samples,
-                                            energy_function.data_ndim,
-                                            self.device)
 
             self.times['eval_bwd_figs_start'] = time()
             bwd_metrics, bwd_fig_dict, rewards, btb_residual, normed_btb_residual = bwd_evaluation(
-                buffer, gfn_model,
-                init_state, eval_discretizer,
+                buffer, gfn_model, eval_discretizer, self.backward_batch_size,
                 do_figs=do_figs)
             self.times['eval_bwd_figs_end'] = time()
+
             metrics.update(bwd_metrics)
             fig_dict.update(bwd_fig_dict)
 
             if buffer.noised_size > 0:
-                # also refresh the buffer
-                alpha = 0.5
-                eps = 0.05
-                beta = 1.0
-
-                score = alpha * stdz(rewards) + (1 - alpha) * stdz(-normed_btb_residual)
-                weight = F.softmax(beta * score, dim=0)
-
-                weight = (1 - eps) * weight + eps / len(weight)
-                weight = weight.clamp(min=0)
-                weight = weight/weight.sum()
-
-                sample_inds = torch.multinomial(weight, num_samples=1000, replacement=True)[:self.args.eval_batch_size] # subsample for speed
-
-                reward_range = 5
-                reward_record, sample_record = noise_buffer(0.5, 1,
-                                                            buffer, energy_function, reward_range,
-                                                            noise_step=0.025,
-                                                            sample_inds=sample_inds)
-
-                reward_record = torch.tensor(reward_record)
-                good_inds = torch.argwhere(reward_record >= buffer.reward_clip).flatten()
-
-                buffer.add_to_noised(reward_record[good_inds],
-                                     torch.stack(sample_record)[good_inds])
+                self.grow_noised_buffer(buffer, energy_function, normed_btb_residual, rewards)
 
         '''logging and wrap up'''
         self.times['eval_wrapup_start'] = time()
@@ -1301,6 +1277,29 @@ class Modeller:
         #                                                              test_mol_loader,
         #                                                              )
         #         metrics.update(conditional_metrics)
+
+    def grow_noised_buffer(self, buffer, energy_function, normed_btb_residual, rewards):
+        self.times['eval_bwd_noising_start'] = time()
+        # also refresh the buffer
+        alpha = 0.5
+        eps = 0.05
+        beta = 1.0
+        score = alpha * stdz(rewards) + (1 - alpha) * stdz(-normed_btb_residual)
+        weight = F.softmax(beta * score, dim=0)
+        weight = (1 - eps) * weight + eps / len(weight)
+        weight = weight.clamp(min=0)
+        weight = weight / weight.sum()
+        sample_inds = torch.multinomial(weight, num_samples=1000, replacement=True)[:self.args.eval_batch_size]  # subsample for speed
+        reward_range = 5
+        reward_record, sample_record = noise_buffer(0.5, 1,
+                                                    buffer, energy_function, reward_range,
+                                                    noise_step=0.05,
+                                                    sample_inds=sample_inds)
+        reward_record = torch.tensor(reward_record)
+        good_inds = torch.argwhere(reward_record >= buffer.reward_clip).flatten()
+        buffer.add_to_noised(reward_record[good_inds],
+                             torch.stack(sample_record)[good_inds])
+        self.times['eval_bwd_noising_end'] = time()
 
     def eval_sampling(self, buffer, energy_function, eval_discretizer, gfn_model, test_mol_loader):
         flow_states_list = []